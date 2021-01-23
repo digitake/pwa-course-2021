60118921375176806c78470372884b69816bb4e1
klongkwan <klongkwan2000@gmail.com>
2021-01-23 22:22:45 +0700

    เพิ่มfont=tahoma เพิ่ม head text เพิ่ม  bg รูป

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Klongkwan <h2>Wanngam</h2>
+        </h1>
         <p>
           Pigleg Got A
         </p>

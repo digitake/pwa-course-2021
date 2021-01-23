d1e1115794884be203e321b570b63de0fc26c2b3
kengpopra <kengpoprasit@gmail.com>
2021-01-23 17:21:40 +0700

    Change Bg color And Text

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World!
         </p>
+        JS
         <a
           className="App-link"
           href="https://reactjs.org"

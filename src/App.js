bd61d8789b389b115979af5590ee714552cc8df3
gaharax <anucha.yutt@bumail.net>
2021-01-23 17:05:27 +0700

    เปลี่ยน สี เเละ ข้อความ

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Error
         </p>
+        ลาก่อน 
         <a
           className="App-link"
           href="https://reactjs.org"

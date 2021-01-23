f171a1b35f8bd529dff22d19d9778393d7fc5ffa
alldog0223 <alldog0223@gmail.com>
2021-01-23 17:00:13 +0700

    แก้สี กับภาษา

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world!
+          i'm Hill
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          จาวาสคริป
         </a>
       </header>
     </div>

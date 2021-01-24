c5f0503ed017ecee531df7157cdf575d8285d911
klongkwan <klongkwan2000@gmail.com>
2021-01-24 14:13:48 +0700

    เล่นเเบบ

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App">    
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Klongkwan Wanngam
-        </h1>
+        <h3>Klongkwan Wanngam</h3>
+        <h2>1620704005</h2>
         <p>
-          Klongkwan Got A
+          Mobile dev
         </p>
         <a
           className="App-link"
@@ -24,5 +23,6 @@ function App() {
     </div>
   );
 }
+// ctrl+alt+p to open command then run npm start to open localhost:300 
 
 export default App;

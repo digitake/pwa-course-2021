da71b4fb7aab0269e6ce66995bb06409821e82b6
settakorn_sris <settakorn.sris@bumail.net>
2021-01-30 17:47:16 +0700

    AppChatDemoSec2511-4153

--- src/App.js
+++ src/App.js
@@ -4,13 +4,81 @@ function App() {
   return (
     <div className="App">
       <div className="App-header">
+        NepTune.Chat
       </div>
+    
       <div className="App-chatroom">
-        <div className="App-chatroom-text">
-          Line1
+        <div>
+          30 Jan
+        <div className="App-chatroom-text-right">
+          <div className="App-chatroom-name">
+          Fourth
+          </div>
         </div>
-        <div className="App-chatroom-text">
-          Line2
+        <div className="App-chatroom-text-right">
+           หวัดดีค้าบจารย์ 
+        </div>
+        </div>
+        <div>
+        <div className="App-chatroom-text-left">
+          <div className="App-chatroom-name">
+          A.Aek
+          </div>
+        </div>
+        <div className="App-chatroom-text-left">
+           ไงครับ ว่าไง
+        </div>
+        </div>
+        <div>
+        <div className="App-chatroom-text-right">
+          <div className="App-chatroom-name">
+          Fourth
+          </div>
+        </div>
+        <div className="App-chatroom-text-right">
+           ผมขอ A นะครับ ผมขก.เรียน
+        </div>
+        </div>
+        <div>
+        <div className="App-chatroom-text-left">
+          <div className="App-chatroom-name">
+          A.Aek
+          </div>
+        </div>
+        <div className="App-chatroom-text-left">
+           ได้ครับ ขอบคุณที่เข้าเรียนนะครับ^^ 
+        </div>
+        </div>
+        16 May
+        <div>
+        <div className="App-chatroom-text-right">
+          <div className="App-chatroom-name">
+          Fourth
+          </div>
+        </div>
+        <div className="App-chatroom-text-right">
+           จารย์ครับทำไมผม F อะ 
+        </div>
+        </div>
+        <div>
+        <div className="App-chatroom-text-right">
+          <div className="App-chatroom-name">
+          Fourth
+          </div>
+        </div>
+        <div className="App-chatroom-text-right">
+           ฮัลโหล๊วววว 
+        </div>
+        </div>
+        <div>
+        <div className="App-chatroom-text-right">
+          <div className="App-chatroom-name">
+          Fourth
+          </div>
+        </div>
+        <div className="App-chatroom-text-right">
+           จ๊ารรร!!!!!!!!!!! 
+        </div>
         </div>
       </div>
       <div className="App-textbox">

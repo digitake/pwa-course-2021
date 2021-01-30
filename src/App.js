93a695708950ea7d1ed2fb277f9120c8d40d28c7
ElijaChinda <Elija.chin@bumail.net>
2021-01-30 09:40:30 +0700

    Change Logo to CPlatoon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './C.png';
 import './App.css';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Elija Chinda
         </a>
       </header>
     </div>

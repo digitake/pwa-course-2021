d7dfcbffb9f9c9c579a18a1f7a4346f470acbf76
adisorn080843 <adisorn.numk@bumail.net>
2021-01-30 09:42:34 +0700

    เปลี่ยนโลโก้เป็นแมว

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './cat.jpg';
 import './App.css';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          จาวาสคริป
+          Mixkey
         </a>
       </header>
     </div>

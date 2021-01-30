97633bdb304afc270feebf68850c9f6296fae902
Assadawut999 <assadawut.hanl@bumail.net>
2021-01-30 11:24:02 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            All Contents Around The World
         </p>
         <a
           className="App-link"

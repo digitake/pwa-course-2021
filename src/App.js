dd60c2a79a8cd009f733b862b9b6b1d7ba5d263a
--global <--global>
2021-01-30 16:58:25 +0700

    ืทำเป็นเหมือนแอป หิวข้างกันมั้ย 5555

--- src/App.js
+++ src/App.js
@@ -4,13 +4,29 @@ function App() {
   return (
     <div className="App">
       <div className="App-header">
+        หิวข้าวกันบ้างมั้ย
       </div>
       <div className="App-chatroom">
         <div className="App-chatroom-text">
-          Line1
+          สวัสดีครับ/ค่ะ
         </div>
         <div className="App-chatroom-text">
-          Line2
+          นี่คือเว็ปหาเพื่อนกินข้าว!!
+        </div>
+        <div className="App-chatroom-text">
+          ขอให้มีอร่อยนะครับ/ค่ะ
+        </div>
+        <div className="App-chatroom-text">
+          [ วิธีเล่น : พิมพ์ข้อความลงไปเลย เพื่อหาเพื่อนที่อยู่ใกล้เรา ]
+        </div>
+        <div>
+          Tu : ร้านไหนมีกระเพราหมูกรอบขายบ้างครับ
+        </div>
+        <div>
+        Pom : ลองไปดูที่โรงอาหาร A3 นะครับ
+        </div>
+        <div>
+        Par : แถวนั้นมีร้านอาหารด้วยหรอค่ะ ว้าววว
         </div>
       </div>
       <div className="App-textbox">

8a56dd349e3472af669ca52a771772fd261c3f0e
janjao93 <janjao937@gmail.com>
2021-01-30 18:19:18 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -8,13 +8,28 @@ function App() {
       <div className="App-chatroom">
         <div className="App-chatroom-text">
           Line1
+          <div className="App-chatroom">
+            Username1
+          </div>
           
         </div>
         <div className="App-chatroom-text">
           Line2
+          <div className="App-chatroom">
+            Username2
+          </div>
         </div>
         <div className="App-chatroom-text">
           Line3
+          <div className="App-chatroom">
+            Username3
+          </div>
+        </div>
+        <div className="App-chatroom-text">
+          Line4
+          <div className="App-chatroom">
+            Username4
+          </div>
         </div>
       </div>
       <div className="App-textbox">

0c48e412d97a5f8b7f89792b74e2e5e34551d475
--set <--set>
2021-01-30 09:34:08 +0700

    เปลี่ยนรูปโลโก้เป็นเป็ด

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './duck.jpg';
 import './App.css';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          จาวาสคริป
+          ทดสอบ
         </a>
       </header>
     </div>

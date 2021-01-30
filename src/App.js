2c06465281c4321ed26d7a49df02d925ef751b8c
Nattakan-phim <Nattakan.phim@gmail.net>
2021-01-30 09:41:45 +0700

    chang image

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './iconfinder_Streamline-78_185098.png';
 import './App.css';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          จาวาส คลิป
         </a>
       </header>
     </div>

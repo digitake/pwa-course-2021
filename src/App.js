4e2af30b3be270b18b1bc47b354404d00d879a1b
ArunsawadBoonthus <Arunsawad.boon@bumail.net>
2021-01-30 09:43:58 +0700

    Change Img

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './DSC02378.jpg';
 import './App.css';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          สวัสดี
         </a>
       </header>
     </div>

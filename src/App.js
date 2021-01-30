084c2fc31d727bc6a7bf6473f396caef8ed1dbad
settakorn_sris <settakorn.sris@bumail.net>
2021-01-30 15:31:36 +0700

    DJB

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          กูกะแล้วตรงนี้ก็อ่าน
         </a>
       </header>
     </div>

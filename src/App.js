087a447f7819e91e3967d5d8cfd7d86cdf277fe1
Assadawut999 <assadawut.hanl@bumail.net>
2021-01-30 11:47:11 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logoNetflix.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            All Contents Around The World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          NETFLIX    
-        </a>
-      </header>
+      <div className="App-header">
+      </div>
+      <div className="App-chatroom">
+        <div className="App-chatroom-text">
+          Line1
+        </div>
+        <div className="App-chatroom-text">
+          Line2
+        </div>
+        <div className="App-chatroom-text">
+          Line3
+        </div>
+        <div className="App-chatroom-text">
+          Line4
+        </div>
+      </div>
+      <div className="App-textbox">
+        <input type="text" className="App-textbox-input"/>
+        <div className="App-textbox-send">Send!</div>
+      </div>
     </div>
   );
 }

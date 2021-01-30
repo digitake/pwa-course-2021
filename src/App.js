c203f6b89016aeb0ea52597471080c4338afe704
netiphochot <neti.phoc@bumail.net>
2021-01-30 17:45:55 +0700

    ปรับแต่ง Chatbox UI เหมือน Facebook Messager App

--- src/App.js
+++ src/App.js
@@ -4,13 +4,38 @@ function App() {
   return (
     <div className="App">
       <div className="App-header">
+        Facebug Messager
       </div>
       <div className="App-chatroom">
         <div className="App-chatroom-text">
-          Line1
+          <div className = "App-chatroom-name-text">
+            Bob
+            </div>
+          girl call me to come over because no one was home.
+        </div>
+        <div className="App-chatroom-sender-text">
+          <div className = "App-chatroom-sender-name-text">
+            Jay
+            </div>
+          so you went to see her but what make you sad bro.
+          <div className = "App-chatroom-seen-text" >
+            SEEN 5:35 PM
+            </div>
         </div>
         <div className="App-chatroom-text">
-          Line2
+          <div className = "App-chatroom-name-text">
+          Bob
+            </div>
+          I went to see her but no one was home.
+        </div>
+        <div className="App-chatroom-sender-text">
+          <div className = "App-chatroom-sender-name-text">
+            Jay
+            </div>
+          ok, that is saddest story I've ever heard.
+          <div className = "App-chatroom-seen-text" >
+            SEEN 5:35 PM
+            </div>
         </div>
       </div>
       <div className="App-textbox">

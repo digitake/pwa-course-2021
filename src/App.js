0406318b96ac0a93f02b1b5bdf814d1107ab86f6
adisorn080843 <adisorn.numk@bumail.net>
2021-01-30 10:20:56 +0700

    ทับ

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
+import logo from './cat.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
-      </div>
-      <div className="App-chatroom">
-        <div className="App-chatroom-text">
-          Line1
-        </div>
-        <div className="App-chatroom-text">
-          Line2
-        </div>
-      </div>
-      <div className="App-textbox">
-        <input type="text" className="App-textbox-input"/>
-        <div className="App-textbox-send">Send!</div>
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          PWA Rocks!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Mixkey
+        </a>
+      </header>
     </div>
   );
 }

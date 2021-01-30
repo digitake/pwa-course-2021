5aef6fd4dba0094b430539fb074bc71b6ddfa1a8
Kittapas24 <kittapas.chan@bumail.net>
2021-01-30 09:40:43 +0700

    เปลี่ยนโลโก้เป็นรูปยุย

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './Yuii.jpg';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          PWA
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          ภาษาไทย
         </a>
       </header>
     </div>

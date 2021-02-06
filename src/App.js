978b4c2ea493229c35286cff7bc597b2025273d5
baimee <vv1oveanime@gmail.com>
2021-02-06 11:45:04 +0700

    ส่งงานคับ

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import './App.css';
+import{useState}from 'react'
 
-function App() {
+function App() 
+{
+
+const [text, setText] = useState("")
+
+const [lines, setLines] = useState(["Let's Talk"]);
+
+const onTextChange = (event) => {
+  setText(event.target.value);
+}
+
+const onSend = (event) =>{
+  setLines(lines => [...lines, text]);
+  setText("");
+}
+
   return (
     <div className="App">
       <div className="App-header">
       </div>
       <div className="App-chatroom">
-        <div className="App-chatroom-text">
-          Line1
+        {
+          lines.map(x =>{
+        return<div className="App-chatroom-text">
+          {x}
         </div>
-        <div className="App-chatroom-text">
-          Line2
-        </div>
-        <div className="App-chatroom-text">
-          Line3
-        </div>
+          })
+        }
+
       </div>
       <div className="App-textbox">
-        <input type="text" className="App-textbox-input"/>
-        <div className="App-textbox-send">Send!</div>
+        <input type="text" className="App-textbox-input" value ={text} onChange= {onTextChange}/>
+        <div className="App-textbox-send" onClick ={onSend}>Send!</div>
       </div>
     </div>
   );

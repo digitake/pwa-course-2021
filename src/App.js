dbef417d0e16d09f7200273923edcef5c4d6eb4b
gaharax <anucha.yutt@bumail.net>
2021-02-06 17:14:58 +0700

    เเก้BG,สี,เพิ่มให้สามารถเเชทได้

--- src/App.js
+++ src/App.js
@@ -1,39 +1,52 @@
 import './App.css';
-
+import {useState} from 'react';
 function App() {
+
+  const [text,setText] = useState("");
+  const [lines,setLines] = useState(["","","",""]);
+  const onTextChange = (event) => {
+    setText(event.target.value);
+  };
+  const onSend =() =>{
+    setLines([...lines, text])
+    setText("");
+  };
+  const keyPress = (event) =>  {
+    if(event.which===13){
+      onSend();
+    }
+  }
   return (
     <div className="App">
       <div className="App-header">
+        Cat Chat
       </div>
       <div className="App-chatroom">
         <div className="App-chatroom-text">
           VIP Live
         </div>
         <div className="App-chatroom-text">
-          Lion
+          Lion 
         </div>
         <div className="App-chatroom-text">
-          Chat
-        </div>
-        
-    
-      </div>
-      <div className="App-chatroom">
-        <div className="App-chatroom-text">
-          VIP Live
+          Cat 
         </div>
         <div className="App-chatroom-text">
-          Lion
+          Fisher Man 
         </div>
         <div className="App-chatroom-text">
-          Chat
+          {
+          lines.map(x => {
+          return <div className="App-chatroom-text">
+                  {x}
+                  </div>
+          })
+          }
         </div>
-        
-    
       </div>
       <div className="App-textbox">
-        <input type="text" className="App-textbox-input"/>
-        <div className="App-textbox-send">ส่ง</div>
+        <input type="text" className="App-textbox-input" value={text} onChange={onTextChange} onKeyPress={keyPress}/>
+        <div className="App-textbox-send" onClick={onSend}>ส่ง</div>
       </div>
     </div>
   );

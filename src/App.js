d97acccf307cb735372d30af7679fd640c4494cf
settakorn_sris <settakorn.sris@bumail.net>
2021-02-06 17:51:18 +0700

    Fix send button and change BG

--- src/App.js
+++ src/App.js
@@ -1,89 +1,42 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [text, setText] = useState("");
+  const [lines, setLines] = useState([]);
+
+  const onTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const onSend = () =>{
+    setLines([...lines, text])
+    setText("");
+  };
+
+  const keyPress = (event) => {
+    if (event.which === 13){
+      onSend();
+    }
+  }
   return (
     <div className="App">
       <div className="App-header">
         NepTune.Chat
       </div>
-    
       <div className="App-chatroom">
-        <div>
-          30 Jan
-        <div className="App-chatroom-text-right">
-          <div className="App-chatroom-name">
-          Fourth
-          </div>
-        </div>
-        <div className="App-chatroom-text-right">
-           หวัดดีค้าบจารย์ 
-        </div>
-        </div>
-        <div>
-        <div className="App-chatroom-text-left">
-          <div className="App-chatroom-name">
-          A.Aek
-          </div>
-        </div>
-        <div className="App-chatroom-text-left">
-           ไงครับ ว่าไง
-        </div>
-        </div>
-        <div>
-        <div className="App-chatroom-text-right">
-          <div className="App-chatroom-name">
-          Fourth
-          </div>
-        </div>
-        <div className="App-chatroom-text-right">
-           ผมขอ A นะครับ ผมขก.เรียน
-        </div>
-        </div>
-        <div>
-        <div className="App-chatroom-text-left">
-          <div className="App-chatroom-name">
-          A.Aek
-          </div>
-        </div>
-        <div className="App-chatroom-text-left">
-           ได้ครับ ขอบคุณที่เข้าเรียนนะครับ^^ 
-        </div>
-        </div>
-        16 May
-        <div>
-        <div className="App-chatroom-text-right">
-          <div className="App-chatroom-name">
-          Fourth
-          </div>
-        </div>
-        <div className="App-chatroom-text-right">
-           จารย์ครับทำไมผม F อะ 
-        </div>
-        </div>
-        <div>
-        <div className="App-chatroom-text-right">
-          <div className="App-chatroom-name">
-          Fourth
-          </div>
-        </div>
-        <div className="App-chatroom-text-right">
-           ฮัลโหล๊วววว 
-        </div>
-        </div>
-        <div>
-        <div className="App-chatroom-text-right">
-          <div className="App-chatroom-name">
-          Fourth
-          </div>
-        </div>
-        <div className="App-chatroom-text-right">
-           จ๊ารรร!!!!!!!!!!! 
-        </div>
-        </div>
+        {
+          lines.map(x => {
+            return <div className="App-chatroom-text">
+              {x}
+            </div>
+          })
+        }
       </div>
       <div className="App-textbox">
-        <input type="text" className="App-textbox-input"/>
-        <div className="App-textbox-send">Send!</div>
+        <input type="text" className="App-textbox-input" value={text} onChange={onTextChange} onKeyPress={keyPress}/>
+        <div className="App-textbox-send" onClick={onSend}>Send</div>
       </div>
     </div>
   );

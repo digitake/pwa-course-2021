01bc504e65a8f34f5e485b181dda1b816a4acdb0
NattaponRP <nattapon.rung@bumail.net>
2021-02-06 11:41:11 +0700

    homework

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [text, setText] = useState("");
+  const [lines, setLines] = useState([""]);
+
+  const onChangeHandler = (event) => {
+      setText(event.target.value);
+  };
+
+  const onSendHandler = (event) => {
+    setLines(lines => [...lines, text]);
+    setText("");
+  };
 
   return (
     <div className="App">
       <div className="App-header">
       </div>
       <div className="App-chatroom">
-        <div className="App-chatroom-text">
-          ข้อความ 1
-        </div>
-        <div className="App-chatroom-text">
-          ข้อความ 2
-        </div>
+        {
+          lines.map((value, index) => {
+            return <div key={index} className="App-chatroom-text">
+                {value}
+              </div>
+          })
+        }
       </div>
       <div className="App-textbox">
-        <input type="text" className="App-textbox-input"/>
-        <div className="App-textbox-send">ส่ง!</div>
+        <input type="text" className="App-textbox-input" onChange={onChangeHandler} value={text}/>
+        <div className="App-textbox-send" onClick={onSendHandler}>ส่ง!</div>
       </div>
     </div>
   );

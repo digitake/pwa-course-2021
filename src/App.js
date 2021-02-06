cd332d9b387e460365576a11f48e0aeb417c11fb
Nattakan-phim <Nattakan.phim@gmail.net>
2021-02-06 11:11:44 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [text, setText] = useState("");
+  const [lines, setLines] = useState(["Hello world!"]);
+
+  const onChangeHandler = (event) => {
+      setText(event.target.value);
+  };
+
+  const onSendHandler = (event) => {
+    setLines(lines => [...lines, text]);
+    setText("");
+  };
+
   return (
     <div className="App">
       <div className="App-header">
@@ -12,10 +26,19 @@ function App() {
         <div className="App-chatroom-text">
           Line2
         </div>
+        {
+          lines.map((value, index) => {
+            return <div key={index} className="App-chatroom-text">
+                {value}
+              </div>
+          })
+        }
       </div>
       <div className="App-textbox">
         <input type="text" className="App-textbox-input"/>
         <div className="App-textbox-send">Send!</div>
+        <input type="text" className="App-textbox-input" onChange={onChangeHandler} value={text}/>
+        <div className="App-textbox-send" onClick={onSendHandler}>Send!</div>
       </div>
     </div>
   );

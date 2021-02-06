7da45b7691b84b23502bd910f914f66c9dc3efc6
KasidtR <kasidit.ruangmunjit@gmail.com>
2021-02-06 16:48:59 +0700

    Week3 พิมข้อความ

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  const [lines, setLines] = useState([]);
+  const onTextChange = (event)=>{
+    setText(event.target.value);
+  };
+  const onSend = () =>{
+    setLines([...lines, text]);
+    setText("");
+  };
+  const keyPress = (event)=>{
+    if(event.which === 13 ){
+      onSend();
+    }
+  }
   return (
     <div className="App">
       <div className="App-header">
       <div className="App-Text">
-          NAME
+          KasiditR
         </div>
       </div>
       <div className="App-chatroom">
         <div className="App-chatroom-text">
-          Line1
+         { lines.map(x =>{
+            return <div className="App-chatroom-text">
+              {x}
+            </div>
+          })
+        }
         </div>
-        <div className="App-chatroom-text">
-          Line2
-        </div>
-        <div className="App-chatroom-text">
-          IAMjohn
-        </div>
-
       </div>
       <div className="App-textbox">
-        <input type="text" className="App-textbox-input"/>
-        <div className="App-textbox-send">Send!</div>
+        <input type="text" className="App-textbox-input" 
+        value={text} onChange={onTextChange} onKeyPress={keyPress}/>
+        <div className="App-textbox-send" onClick={onSend} >Send!</div>
       </div>
     </div>
   );

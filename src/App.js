c9c6b3a85d931a64ce91466e47002a032a13dce8
NitithornRm <nitithorn.rutm@bumail.net>
2021-02-06 11:14:39 +0700

    change code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
+import logo from './dog.jpg';
 import './App.css';
+import { useState, usestate } from 'react';
 
 function App() {
+
+  const [text, setText] = useState("")
+
+  const onTextChange = (event) => {
+    setText(event.target.value)
+  }
+
+  const [textLines, setTextLines] = useState([]);
+
+  const onSend = () => {
+    setTextLines(textLines => [...textLines,text])
+    setText("");
+  }
+
   return (
     <div className="App">
       <div className="App-header">
       </div>
       <div className="App-chatroom">
+        {
+          textLines.map(x=>{
+            return <div className="App-chatroom-text">
+              {x}
+            </div>
+            
+          })
+        }
         <div className="App-chatroom-text">
-          Line1
+          {text}
         </div>
-        <div className="App-chatroom-text">
-          Line2
-        </div>
-        <div className="App-chatroom-text">
-          Line3
-        </div>
-        <div className="App-chatroom-text">
-          Line4
-        </div>
+        
       </div>
       <div className="App-textbox">
-        <input type="text" className="App-textbox-input"/>
-        <div className="App-textbox-send">Send!</div>
+        <input type="text" className="App-textbox-input"value={text} onChange={onTextChange}/>
+        <div className="App-textbox-send"onClick={onSend}>   Send!</div>
       </div>
     </div>
   );

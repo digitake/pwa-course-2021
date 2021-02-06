921bdac592def29c0cc45b25cec753b6df50535c
NattaponRP <nattapon.rung@bumail.net>
2021-02-06 11:14:19 +0700

    text assigh

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import './App.css';
 
 function App() {
+
+  const [text, setText] = useState("x");
+
+  const [lines, setLines] = useState(["msg1" , "msg2", "msg3" , "msg4"]);
+
+  const onTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const onSend = () => {
+    setLines(lines => [... lines, text]);
+    setText("");
+  }
+
+
   return (
     <div className="App">
       <div className="App-header">
       </div>
       <div className="App-chatroom">
+        {
+          lines.map(x =>{
+            return <div className="Appchatroom-text">
+              {x}
+              </div>
+          })
+        }
         <div className="App-chatroom-text">
-          ข้อความ 1
+          
         </div>
-        <div className="App-chatroom-text">
-          ข้อความ 2
-        </div>
+
       </div>
       <div className="App-textbox">
-        <input type="text" className="App-textbox-input"/>
+        <input type="text" className="App-textbox-input" value={text} onChange={onTextChange}/>
         <div className="App-textbox-send">ส่ง!</div>
       </div>
     </div>

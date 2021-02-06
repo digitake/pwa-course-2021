6632923847d2541e25828376fc157e46eaf8aeab
Thanaphat1150 <thanaphat.sang@bumail.net>
2021-02-06 12:36:35 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+const [text , setText] = useState("");
+
+const [Lines, SetLine] = useState([]);
+
+const onTextChange = (event) =>{
+  setText(event.target.value)
+};
+
+const onSent = () => {
+  SetLine(Line => [...Lines, text])
+  setText("");
+}
   return (
     <div className="App">
       <div className="App-header">
       </div>
       <div className="App-chatroom">
-        <div className="App-chatroom-text">
-          Line1
-        </div>
-        <div className="App-chatroom-text">
-          Line2
-        </div>
-        <div className="App-chatroom-text">
-          Line3
-        </div>
-        <div className="App-chatroom-text">
-          Line4
-        </div>
-        <div className="App-chatroom-text">
-          Line5
-        </div>
+        {
+          Lines.map(x =>{
+          return <div className="App-chatroom-text">
+                {x}
+            </div>
+          })
+        }
       </div>
       <div className="App-textbox">
-        <input type="text" className="App-textbox-input"/>
-        <div className="App-textbox-send">Send!</div>
+        <input type="text" className="App-textbox-input"value={text} onChange={onTextChange}/>
+        <div className="App-textbox-send" onClick={onSent}>Send!</div>
       </div>
     </div>
   );

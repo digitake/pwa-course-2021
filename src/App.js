bed48f124662ae8e31ad4901c1248182d2da75bf
Noraphat-thod <noraphat.thod@bumail.net>
2021-02-06 10:21:03 +0700

    class 3

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import logo from './ss02.png';
 import './App.css';
+import { useState } from 'react'
+
 
 function App() {
+
+  const [text, setText] = useState("")
+
+  const onTextChange = (event) => {
+    setText(event.target.value);
+  };
+
   return (
     <div className="App">
       <div className="App-header">
       </div>
       <div className="App-chatroom">
         <div className="App-chatroom-text">
-          Line1
+          {text}
         </div>
         <div className="App-chatroom-text">
-          Line2
+          {text}
         </div>
       </div>
       <div className="App-textbox">
-        <input type="text" className="App-textbox-input"/>
+        <input type="text" className="App-textbox-input" value={text} onChange={onTextChange}/>
         <div className="App-textbox-send">Send!</div>
       </div>
     </div>

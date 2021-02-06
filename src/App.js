792bca629938ca943e118346fd643d7863ed91a7
Assadawut999 <assadawut.hanl@bumail.net>
2021-02-06 10:47:58 +0700

    เเก้ pull code ครั้งที่2 คลาสที่3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+const [text, setText] = useState("asdasda");
+
+const onTextChange = (event) => {
+  setText(event.target.value)
+}
   return (
     <div className="App">
       <div className="App-header">
       </div>
       <div className="App-chatroom">
         <div className="App-chatroom-text">
-          Line1
+         {text}
         </div>
         <div className="App-chatroom-text">
-          Line2
-        </div>
-        <div className="App-chatroom-text">
-          Line3
-        </div>
-        <div className="App-chatroom-text">
-          Line4
         </div>
       </div>
       <div className="App-textbox">
-        <input type="text" className="App-textbox-input"/>
+        <input type="text" className="App-textbox-input" value = {text} onChange = {onTextChange}/> 
         <div className="App-textbox-send">Send!</div>
       </div>
     </div>

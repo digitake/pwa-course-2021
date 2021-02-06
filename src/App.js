e64b519876dbe207fdab9519193170b7663b0e39
kennykung2 <thanakrit.siri@bumail.net>
2021-02-06 13:13:14 +0700

    weak3

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [text, setText] = useState("");
+
+  const [lines, setLines] = useState(["msg1","msg2","msg3","msg4","msg5"]);
+
+  const onTextChange = (event) => {
+    setText(event.target.value);
+  }; 
+  
+  const onSend = (event) => {
+    setLines(lines => [...lines, text]);
+    setText("");
+  };
+
   return (
     <div className="App">
       <div className="App-header">
+        Daring
       </div>
       <div className="App-chatroom">
-        <div className="App-chatroom-text">
-          Line1
-        </div>
-        <div className="App-chatroom-text">
-          Line2
-        </div>
+        {
+          lines.map(x =>{
+            return <div className="App-chatroom-text">
+              {x}
+              </div>
+            } )
+        }
       </div>
       <div className="App-textbox">
-        <input type="text" className="App-textbox-input"/>
-        <div className="App-textbox-send">Send!</div>
+        <input type="text" className="App-textbox-input" value = {text} onChange = {onTextChange}/>
+        <div className="App-textbox-send" onClick = {onSend}>Send!</div>
       </div>
     </div>
   );

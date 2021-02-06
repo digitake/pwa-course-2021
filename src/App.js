c206e4a10fd6a15cad0f3d37af3f72ba92abff38
adisorn080843 <adisorn.numk@bumail.net>
2021-02-06 11:36:26 +0700

    แก้โค้ด week 3

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [text, setText] = useState("");
+  const [lines, setLines] = useState(["msg1!"]);
+
+  const onChangeHandler = (event) => {
+    setText(event.target.value);
+  };
+
+  const onSendHandler = (event) => {
+    setLines(lines => [...lines, text]);
+    setText("");
+  };
+
   return (
     <div className="App">
       <div className="App-header">
       </div>
       <div className="App-chatroom">
-        <div className="App-chatroom-text">
-          Line1
-        </div>
-        <div className="App-chatroom-text">
-          Line2
-        </div>
-        <div className="App-chatroom-text">
-          Line3
-        </div>
-        <div className="App-chatroom-text">
-          Line4
-        </div>
+      {
+          lines.map((value, index) => {
+            return <div key={index} className="App-chatroom-text">
+                {value}
+                </div>
+          })
+        }
       </div>
       <div className="App-textbox">
-        <input type="text" className="App-textbox-input"/>
-        <div className="App-textbox-send">Send!</div>
+      <input type="text" className="App-textbox-input" onChange={onChangeHandler} value={text}/>
+      <div className="App-textbox-send" onClick={onSendHandler}>Send!</div>
       </div>
-    </div>
+      </div>
   );
 
 }

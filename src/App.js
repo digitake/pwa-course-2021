2c79c6376f3da2710a846e84ed001a6c047c53e7
lapassuth <lapas.suth@bumail.net>
2021-02-06 17:11:58 +0700

    เปลี่ยน background และใส่โค้ดให้พิมพ์ได้

--- src/App.js
+++ src/App.js
@@ -1,30 +1,61 @@
 import './App.css';
-
+import {useState} from 'react';
 function App() {
+
+  const [text,setText] = useState("");
+  const [lines,setLines] = useState([
+    "","","",""]);
+  const onTextChange = (event) => {
+    setText(event.target.value);
+  };
+  const onSend =() =>{
+    setLines([...lines, text])
+    setText("");
+  };
+  const keyPress = (event) =>  {
+    if(event.which===13){
+      onSend();
+    }
+  }
   return (
     <div className="App">
       <div className="App-header">
       </div>
       <div className="App-chatroom">
         <div className="App-chatroom-text">
-          สองเมียสี่
+          พิมข้อความ
         </div>
         <div className="App-chatroom-text">
-          มาร์คบางระจัน
+          
         </div>
         <div className="App-chatroom-text">
-          ชาชงกาแฟ
+          {
+          lines.map(x => {
+          return <div className="App-chatroom-text">
+                  {x}
+                  </div>
+          })
+          }
+        </div>
+
+
+      </div>
+      <div className="App-chatroom">
+        <div className="App-chatroom-text">
+          
         </div>
         <div className="App-chatroom-text">
-          ภัสโสดค้าบ
+          
         </div>
         <div className="App-chatroom-text">
-          เฟิสสเปิ๊ดก๊าด
+          
         </div>
+
+
       </div>
       <div className="App-textbox">
-        <input type="text" className="App-textbox-input"/>
-        <div className="App-textbox-send">Send!</div>
+        <input type="text" className="App-textbox-input" value={text} onChange={onTextChange} onKeyPress={keyPress}/>
+        <div className="App-textbox-send" onClick={onSend}>ส่งข้อความ</div>
       </div>
     </div>
   );

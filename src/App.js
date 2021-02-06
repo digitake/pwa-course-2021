080e7db16b8ccbbb4d310977f5f2fd04ced66284
Nattakan-phim <Nattakan.phim@gmail.net>
2021-02-06 11:18:20 +0700

    up datecode

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
 
   const [text, setText] = useState("");
-  const [lines, setLines] = useState(["as" ,"sdad"]);
+  const [lines, setLines] = useState(["sdad","dasd"]);
 
   const onChangeHandler = (event) => {
       setText(event.target.value);
   };
 
   const onSendHandler = (event) => {
-    setLines(lines => [lines, text]);
+    setLines(lines => [...lines, text]);
     setText("");
   };
+
 
   return (
     <div className="App">
@@ -36,7 +38,7 @@ function App() {
       </div>
       <div className="App-textbox">
         <input type="text" className="App-textbox-input" onChange={onChangeHandler} value={text}/>
-        <div className="App-textbox-send">Send!</div>
+        <div className="App-textbox-send" onClick={onSendHandler}>Send!</div>
       </div>
     </div>
   );

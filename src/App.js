483074464aa8a1f42822557a7cc3eb83a8a3cfeb
Destiny-netizen <chattapat.sant@bumail.net>
2021-02-11 10:57:50 +0700

    https://github.com/Destiny-netizen/pwa-course-2021

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  
+  const [text, setText] = useState("");
+
+  const [lines, setLines] = useState(["mschat1","mschat2","mschat3"]);
+
+  const onTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+const onSend = () => {
+  setLines(lines => [...lines, text]);
+  setText("");
+}
   return (
     <div className="App">
       <div className="App-header">
       </div>
       <div className="App-chatroom">
-        <div className="App-chatroom-text">
-          Line1
-        </div>
-        <div className="App-chatroom-text">
-          Line2
-        </div>
-        <div className="App-chatroom-text">
-          Line3
-        </div>
-        <div className="App-chatroom-text">
-          Line4
-        </div>
-        <div className="App-chatroom-text">
-          Line5
-        </div>
+        {
+          lines.map(x=>{
+            return <div className= "App-chatroom-text">
+              {x}
+              </div>
+          })
+        }
+        
       </div>
       <div className="App-textbox">
-        <input type="text" className="App-textbox-input"/>
-        <div className="App-textbox-send">Send!</div>
+        <input type="text" className="App-textbox-input" value={text} onChange={onTextChange}/>
+        <div className="App-textbox-send"onClick={onSend}>Send!</div>
       </div>
     </div>
   );

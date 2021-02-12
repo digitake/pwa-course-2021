b4261e6b0c0a1a32906944439460b1a90eacb79c
OosnowoO <konan2536@hotmail.com>
2021-02-12 07:51:34 +0700

    เปลี่ยนสีพื้อนหลัง และ เปลี่ยนข้อความ

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [text , setText ] = useState("");
+  const [lines, setLines] = useState(["msg1","msg2","msg3"]);
+
+  const onTextChange = (event) => {setText(event.target.value);
+  };
+
+  const onSend = () => {setLines ([...lines, text]);
+  setText("");
+};
+
+const keyPress = (event) => {if (event.which === 13){onSend();}
+  
+};
+ 
   return (
     <div className="App">
       <div className="App-header">
       </div>
+      <div className ="panel-footer">
+      </div>
+      <div className ="input-group">
+      </div>
       <div className="App-chatroom">
         <div className="App-chatroom-text">
-          Chat Group
-        </div>
-          
+          {lines.map(x => {return <div className="App-chatroomn-text">{x}</div>})}
       </div>
-      <div className="App-textbox">
-        <input type="text" className="App-textbox-input"/>
-        <div className="App-textbox-send">Send!</div>
       </div>
+          <input id = "btn-input" type="text" className="form-control-input-sm" value={text} onChange={onTextChange}  onKeyPress={keyPress}
+          placeholder="Type your massage here...." />
+          <span className="input-group-btn">
+            <button className = "btnbtn-primarybtn-sm" id="btn-chat" onClick={onSend}>Send your massage</button>
+          </span>
     </div>
   );
 }

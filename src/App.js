d868f0f93b5ba770f4ced973b50666542f797e20
Kittapas24 <kittapas.chan@bumail.net>
2021-02-13 11:16:39 +0700

    debug code

--- src/App.js
+++ src/App.js
@@ -23,20 +23,20 @@ function App() {
       </div>
       <div className="App-chatroom">
         {
-          lines.map(x =>{
-            return<div className="App-chatroom-text">
-              {x}
-              </div>
+          lines.map(x => {
+            return <div className="App-chatroom-text">
+                    {x}
+                   </div>
           })
-         }
-        </div>
+        }
       </div>
       <div className="App-textbox">
-        <input type="text" className="App-textbox-input" value = {text} onChange={onTextChange}/>
+        <input type="text" className="App-textbox-input" 
+        value={text} onChange={onTextChange} onKeyPress={keyPress}/>
+        
         <div className="App-textbox-send" onClick={onSend}>Send!</div>
       </div>
     </div>
   );
-}
 
 export default App;

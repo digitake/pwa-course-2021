f0903a6458cefca8ca696e039e4ab7685c0e9eee
Arunsawad_Boonthus <Arunsawad.boon@bumail.net>
2021-02-20 10:37:37 +0700

    no

--- src/App.js
+++ src/App.js
@@ -22,23 +22,7 @@ function App() {
 
   return (
     <div className="App">
-      <div className="App-header">
-      </div>
-      <div className="App-chatroom">
-        {
-          lines.map(x => {
-            return <div className="App-chatroom-text">
-                    {x}
-                   </div>
-          })
-        }
-      </div>
-      <div className="App-textbox">
-        <input type="text" className="App-textbox-input" 
-        value={text} onChange={onTextChange} onKeyPress={keyPress}/>
-        
-        <div className="App-textbox-send" onClick={onSend}>Send!</div>
-      </div>
+      <div className=""
     </div>
   );
 }

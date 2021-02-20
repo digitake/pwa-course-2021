67d36cc4e6a7cc19cb3e429a82c3ea1727402fe2
Wisarut_Payonpaiboonsuk <wisarut.payo@bumail.net>
2021-02-20 11:03:54 +0700

    Assignment03_1,2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
 
     const [text,setText] = useState("");
 
-    const [lines,setLines] = useState(["msg1"]);
+    const [lines,setLines] = useState([]);
 
     const onTextChange = (event) => {
       setText(event.target.value);
@@ -14,6 +14,12 @@ function App() {
     const onSend =() => {
       setLines(lines => [...lines,text]);
       setText("");
+    };
+
+    const keyPress = (event) => {
+      if (event.which === 13){
+        onSend();
+      }
     };
 
   return (
@@ -30,8 +36,10 @@ function App() {
         } 
       </div>
       <div className="App-textbox">
-        <input type="text" className="App-textbox-input" value = {text} onChange={onTextChange} />
-        <div className="App-textbox-send" onClick = {onSend}>Send!</div>
+      <input type="text" className="App-textbox-input" 
+        value={text} onChange={onTextChange} onKeyPress={keyPress}/>
+
+        <div className="App-textbox-send" onClick={onSend}>Send!</div>
       </div>
     </div>
   );

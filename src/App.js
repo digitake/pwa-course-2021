c038bc68aa76140477de5c35b0c0c602aa8cfcee
AphinanCharoenporn <Aphinan.char@bumail.net>
2021-02-20 11:09:55 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,50 +1,27 @@
 import { useState } from 'react';
 import './App.css';
-import {useState} from 'react';
 
 function App() {
-<<<<<<< HEAD
   const[text, setText] = useState("");
 
   const [lines, setLines] = useState(["Hello KumFile", "How are you?", "Right now I stay at capital city", "I relexed from my traveling", "I will Say Hi again later!!!!!!jajaja hahaha hahah"]);
 
 
-=======
-  const [text, setText] = useState("");
-  const [lines, setLines] = useState([]);
-  
->>>>>>> 0e288e4226d73217af9418f2059f16c9cf3f2dc2
   const onTextChange = (event) => {
     setText(event.target.value);
   };
 
-<<<<<<< HEAD
   const onSend = () => {
     setLines(lines => [...lines, text]);
     setText("");
   };
 
-=======
-  const onSend = () =>{
-    setLines([...lines, text]);
-    setText("");
-  };
-
-  const keyPress = (event) => {
-    if (event.which === 13){
-      onSend();
-    }
-  };
-
->>>>>>> 0e288e4226d73217af9418f2059f16c9cf3f2dc2
   return (
-    <div className="App col-6">
-      <div className="App-header">
+    <div className="App">
         
       </div>
       <div className="App-chatroom">
         {
-<<<<<<< HEAD
 
           lines.map (x=>{
             return <div className= "App-chatroom-text">
@@ -57,20 +34,6 @@ function App() {
       <div className="App-textbox">
         <input type="text" className="App-textbox-input" value = {text} onChange ={onTextChange}/>
         <div className="App-textbox-send" onClick = {onSend}></div>
-=======
-          lines.map(x => {
-            return <div className="App-chatroom-text">
-                    {x}
-                   </div>
-          })
-        }
-      </div>
-      <div className="App-textbox">
-        <input type="text" className="App-textbox-input" 
-        value={text} onChange={onTextChange} onKeyPress={keyPress}/>
-        
-        <div className="App-textbox-send" onClick={onSend}>Send!</div>
->>>>>>> 0e288e4226d73217af9418f2059f16c9cf3f2dc2
       </div>
     </div>
   );

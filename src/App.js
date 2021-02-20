bbec7856cc0fbece2a5937f4a528889f0f01a0c9
jay <jayjay958@gmail.com>
2021-02-20 17:13:32 +0700

    Neptune chat design

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import './App.css';
-import {useState} from 'react';
+import { useState } from 'react';
 
 function App() {
 
-  const [text, setText] = useState("Type something...");
+  const [text, setText] = useState('');
   const [lines, setLines] = useState([]);
 
   const onSend = () => {
     setLines([...lines, text]);
-    setText ("");
+    setText("");
   }
 
   const onTextChange = (event) => {
     setText(event.target.value);
   };
 
-  const onKeyPress = (event) =>{
-    if(event.which === 13){
+  const onKeyPress = (event) => {
+    if (event.which === 13) {
       onSend();
     }
   }
@@ -24,20 +24,20 @@ function App() {
   return (
     <div className="App">
       <div className="App-header">
-        FaceBug
+        Neptune Chat
       </div>
       <div className="App-chatroom">
-       {
-         lines.map  (x => {
-          return <div className="App-chatroom-text">
-          {x}
-        </div>
-         })
-       }
+        {
+          lines.map(msg => {
+            return <div key={msg} className="App-chatroom-text">
+              {msg}
+            </div>
+          })
+        }
 
       </div>
       <div className="App-textbox">
-        <input type="text" className="App-textbox-input" value={text} onKeyPress={onKeyPress} onChange={onTextChange}/>
+        <input placeholder='Type something...' type="text" className="App-textbox-input" value={text} onKeyPress={onKeyPress} onChange={onTextChange} />
         <div className="App-textbox-send" onClick={onSend}>Send!</div>
       </div>
     </div>

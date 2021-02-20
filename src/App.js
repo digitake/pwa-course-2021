615aecdd86e66f6723a9cd6fb8659c432354d1fe
ThanatipLnwza <Thanatip.ngan@bumail.net>
2021-02-20 11:01:59 +0700

    add assign code

--- src/App.js
+++ src/App.js
@@ -1,45 +1,43 @@
+import React, { useState } from 'react';
 import './App.css';
-import {useState} from 'react';
+
 
 function App() {
-  const [text, setText] = useState("");
-  const [lines, setLines] = useState([]);
-  
+
+  const [text, setText] = useState("")
+
+  const [lines, setLines] = useState(["msg1" , "msg2" , "msg3" , "msg4" , "msg5"]);
+
   const onTextChange = (event) => {
     setText(event.target.value);
   };
 
-  const onSend = () =>{
-    setLines([...lines, text]);
+  const onSend = () => {
+    setLines(lines => [...lines, text]);
     setText("");
-  };
-
-  const keyPress = (event) => {
-    if (event.which === 13){
-      onSend();
-    }
   };
 
   return (
     <div className="App">
       <div className="App-header">
+        Chatroom
       </div>
       <div className="App-chatroom">
-        {
-          lines.map(x => {
-            return <div className="App-chatroom-text">
-                    {x}
-                   </div>
-          })
-        }
+      {
+        lines.map(x =>{
+          return <div className="App-chatroom-text">
+                  {x}
+                  </div>
+        }) 
+      }
+    
       </div>
       <div className="App-textbox">
-        <input type="text" className="App-textbox-input" 
-        value={text} onChange={onTextChange} onKeyPress={keyPress}/>
-        
-        <div className="App-textbox-send" onClick={onSend}>Send!</div>
+        <input type="text" className="App-textbox-input" value={text} onChange={onTextChange}/>
+        <div className="App-textbox-send" onClick={onSend}>Send</div>
       </div>
     </div>
+    
   );
 }
 

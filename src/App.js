e7599fce95a4d3b0aef02c336805893c4c8f1e20
winnerprmm <winner.prm@gmail.com>
2021-02-26 17:35:40 +0700

    รวม

--- src/App.js
+++ src/App.js
@@ -18,12 +18,12 @@ function App() {
     if (event.which === 13){
       onSend();
     }
+
   };
 
   return (
     <div className="App">
-      <div className="App-header">
-      </div>
+      <div className="App-header">Receiver's Name</div>
       <div className="App-chatroom">
         {
           lines.map(x => {
@@ -33,11 +33,15 @@ function App() {
           })
         }
       </div>
+      <div className="App-textbox-icon" ><i class="fas fa-microphone-alt"></i> 
+      <i class="fas fa-file-upload hide-when-small"></i>
+      <i class="fas fa-plus hide-when-big"></i>
+      <i class="fas fa-images hide-when-small"></i> 
+      <i class="fas fa-camera"></i> </div>
       <div className="App-textbox">
         <input type="text" className="App-textbox-input" 
         value={text} onChange={onTextChange} onKeyPress={keyPress}/>
-        
-        <div className="App-textbox-send" onClick={onSend}>Send!</div>
+        <div className="App-textbox-send" onClick={onSend}> <i class="far fa-paper-plane"></i> </div>
       </div>
     </div>
   );

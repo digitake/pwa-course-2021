4e9f38a96a6866d513f083763c2ded9c72fb90f3
TanapatSuntornsalitkul <tanapat.sunt@bumail.net>
2021-02-26 23:02:26 +0700

    Chi CHi Responsive UI Chat page

--- src/App.js
+++ src/App.js
@@ -21,11 +21,17 @@ function App() {
     }
   };
 
+
   return (
-    <div className="App col-6">
+    <div className="App">
       <div className="App-header">
-        
+
+      <div className="App-header1" onClick={onSend}></div>
+      <div className="App-header2" onClick={onSend}></div>
+      <div className="App-header3" onClick={onSend}></div>  
+
       </div>
+      
       <div className="App-chatroom">
         {
           lines.map(x => {
@@ -38,8 +44,12 @@ function App() {
       <div className="App-textbox">
         <input type="text" className="App-textbox-input" 
         value={text} onChange={onTextChange} onKeyPress={keyPress}/>
-        
+
         <div className="App-textbox-send" onClick={onSend}></div>
+        <div className="App-textbox-send1" onClick={onSend}></div>
+        <div className="App-textbox-send2" onClick={onSend}></div>
+        
+
       </div>
     </div>
   );

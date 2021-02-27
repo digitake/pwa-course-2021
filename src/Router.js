ab95a94b5dc3987bd20af0fd82d40fef2df7f10b
--global <--global>
2021-02-27 15:45:29 +0700

    ติดตั้ง Router เพื่อชี้ไปที่ App

---
+++ src/Router.js
@@ -0,0 +1,17 @@
+/* import router components เข้ามาเพื่อเตรียมใช้งาน */
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
+import App from './App';
+
+function Router(){
+    return(
+    <BrowserRouter>
+        <Switch>
+            <Route path="/">
+                <App/>
+            </Route>
+        </Switch>
+    </BrowserRouter>
+    )
+};
+
+export default Router; //ส่งออก Router ไปให้โลกใช้งาน 

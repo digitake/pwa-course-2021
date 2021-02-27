0693d7ee77d78902879bc948b0d67c69adc2c2f4
janjao93 <janjao937@gmail.com>
2021-02-27 15:45:37 +0700

    ติดตั้งRouter

---
+++ src/Router.js
@@ -0,0 +1,22 @@
+import{BrowserRouter,Switch,Route} from 'react-router-dom';
+import App from './App';
+
+function Router(){
+
+return(
+    <BrowserRouter>
+    <switch>
+<Route path="/">
+<App/>
+</Route>
+
+
+    </switch>
+    
+    
+    
+    
+    </BrowserRouter>
+)
+};
+export default Router;

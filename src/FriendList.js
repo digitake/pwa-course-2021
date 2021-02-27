07eb161b430c11eda8e865dcc0b87139eb9f4554
baimee <vv1oveanime@gmail.com>
2021-02-27 10:45:31 +0700

    Friendlist add link

--- src/FriendList.js
+++ src/FriendList.js
@@ -1,7 +1,12 @@
+import {Link} from 'react-router-dom';
+
 function FriendList() {
     return (
         <div>
             Friend list
+            <Link to ="/">
+                <img src = "lel.jpg"/>
+            </Link>
             </div>
     );
 };

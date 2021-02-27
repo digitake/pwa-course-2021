aafef81c63dcea2fb561c4d95026cefa4708a858
nottae12 <pannathat.lima@bumail.net>
2021-02-27 17:59:16 +0700

    Profile เสร็จแล้ว

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,14 @@
 import {RouterBrowser,Switch,Route, BrowserRouter} from 'react-router-dom';
 import App from './App';
+import MainPage from './MainPage' ;
 
 function Router() {
     return(
         <BrowserRouter>
             <Switch>
+                <Route path="/a">
+                    <MainPage />
+                </Route>
                 <Route path="/">
                     <App />
                 </Route>

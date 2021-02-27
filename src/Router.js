5001e8513827690299fb94dfc1b9a7cf5e72339e
chatchaiphat924 <chatchiphat@outlook.com>
2021-02-27 10:01:17 +0700

    เพิ่ม Router.js

---
+++ src/Router.js
@@ -0,0 +1,17 @@
+import{BrowserRouter,Switch,Route} from 'react-router-dom';
+import App from './App';
+
+function Router( ) {
+
+    return(
+        <BrowserRouter>
+         <Switch>
+          <Route path="/">
+              <App />
+          </Route>
+         </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Router;

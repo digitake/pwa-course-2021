ce95ebe5174867abf428edbc7d5500ee2d1f311f
Ridakami <Weerawat.chai@bumail.net>
2021-02-27 10:24:10 +0700

    Chat

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,14 @@
 import { browserRouter, Switch, Router } from 'react-router-dom';
 import App from './App';
+import FriendList from './FriendList';
 
 function Router() {
     return (
         <BrowserRouter>
             <Switch>
+                <Route path="/Friend-List">
+                    <FriendList />
+                        </Route>
                 <Route path="/">
                     <App />
                 </Route>

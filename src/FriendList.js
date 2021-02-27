56cb3385eb9063896206697db886fcf47a36376a
Noraphat_Thodsadee <noraphat.thod@bumail.net>
2021-02-27 13:46:16 +0700

    fb

--- src/FriendList.js
+++ src/FriendList.js
@@ -4,6 +4,9 @@ import { Link } from "react-router-dom";
 function FriendList(){
     return(
         <div>
+            <div className="App-header App-logo ">
+
+            </div> 
             Friend List
             <Link to="/">
               <button>

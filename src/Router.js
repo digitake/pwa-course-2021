0c8317e2d6ca75e8964a2bbfededddee4aadb8e0
DESKTOP-C86V8AI\User <pongtanat.sira@bumail.net>
2021-02-27 18:04:00 +0700

    HomePage

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Switch, Route} from 'react-router-dom' ;
 import App from './App';
 import FriendList from './FriendList';
+import HomePage from './HomePage';
 
 function Router() {
     return (
@@ -8,6 +9,9 @@ function Router() {
         <Switch>
             <Route path="/friend-list">
                 <FriendList />
+            </Route>
+            <Route path="/home-page">
+                <HomePage />
             </Route>
             <Route path="/">
                 <App />

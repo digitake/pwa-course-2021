8122b234b0b98275dba23dc220a3e0ae0bbe33c5
mantawit_ramsomphop <mantawit.rams@bumail.net>
2021-02-27 11:07:47 +0700

    เพิ่มปุ่มไป chat

--- src/FriendList.js
+++ src/FriendList.js
@@ -1,8 +1,12 @@
+import { Link } from 'react-router-dom'
 
 function FriendList() {
     return(
         <div>
             FriendList
+            <Link to="/">
+                <img src="hi2.jpg"/>
+            </Link>
         </div>
     );
 };

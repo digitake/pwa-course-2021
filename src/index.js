f312f6a6e414d466b7c11d705e84073b42c1c885
TanapatSuntornsalitkul <tanapat.sunt@bumail.net>
2021-02-27 10:00:31 +0700

    เพิ่ม Router.js ได้แล้ว

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Router from './Router';
 import reportWebVitals from './reportWebVitals';
 
+/** create Conponent */
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>,
   document.getElementById('root')
 );

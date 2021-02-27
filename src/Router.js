8cf4613e9da6b99ad0c7d64435e04948c4aac214
chatchaiphat924 <chatchiphat@outlook.com>
2021-02-27 10:21:53 +0700

    เพิ่มหน้าแรก

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,18 @@
 import{BrowserRouter,Switch,Route} from 'react-router-dom';
 import App from './App';
+import FriendList from './FriendList';
 
 function Router( ) {
 
-    return(
+    return (
         <BrowserRouter>
          <Switch>
-          <Route path="/">
-              <App />
-          </Route>
+             <Route path="/friend-list">
+                 <FriendList />
+             </Route>
+             <Route path="/">
+                <App />
+             </Route>
          </Switch>
         </BrowserRouter>
     )

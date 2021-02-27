b6d0d44dd05faa9a8bb7b3908f886d31d3615bd7
Assadawut_Hanlakorn <assadawut.hanl@bumail.net>
2021-02-27 10:59:21 +0700

    FriendListAdd_img

--- src/FriendList.js
+++ src/FriendList.js
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
 
 function FriendList(){
     return(
         <div>
             Friend List
+            <Link to="/">
+                <img src = "logoNetflix.png"/>
+            </Link>
         </div>
     );
 };

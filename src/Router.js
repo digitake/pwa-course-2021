a48bb1974aebc650c53f70f0fda5cbf7940a8d48
TeerathepS <teerathep.saia@bumail.net>
2021-02-27 09:40:03 +0700

    เพิ่มไฟล์ router.js เข้ามา

---
+++ src/Router.js
@@ -0,0 +1,14 @@
+﻿import {BrowserRouter, Switch, Route} from "react-router-dom";
+import App from "./App";
+
+function Router() {
+    return(
+        <BrowserRouter>
+          <Switch>
+              <Route path="/">
+                  <App />
+              </Route>
+          </Switch>  
+        </BrowserRouter>
+    )
+}

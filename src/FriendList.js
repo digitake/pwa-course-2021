5af4c2752dc9891faf8c258393090f4ff3c468c3
NattaponRP <nattapon.rung@bumail.net>
2021-02-28 00:50:18 +0700

    Friend List Complete

--- src/FriendList.js
+++ src/FriendList.js
@@ -8,7 +8,15 @@ return (
     <div className="friendList">
        <div class="friendList-header">
            <div class="friendList-center">
-               
+               <div class="friendList-friend">
+                <div class="item1">User.1</div>
+                <div class="item2">User.2</div>
+                <div class="item3">User.3</div>
+                <div class="item4">User.4</div>
+                <div class="item4">User.5</div>
+                <div class="item4">User.6</div>
+                <div class="item4">User.7</div>
+               </div>
            </div>
            <div className="friendlist">
                <Link to="/">

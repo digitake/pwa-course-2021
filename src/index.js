3df4bc7b3c36ea145420eef8c706d661ece8d6d9
Kittapas24 <kittapas.chan@bumail.net>
2021-02-27 10:00:00 +0700

    ทำการเพิ่ม Router.js ได้แล้ว

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Rounter from './Rounter';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Rounter />
   </React.StrictMode>,
   document.getElementById('root')
 );

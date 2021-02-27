7cf1399a66448998fd6c0465dea439c6ca8933c5
NattaponRP <nattapon.rung@bumail.net>
2021-02-27 16:28:50 +0700

    Add Back_Button + Header

--- src/FriendList.js
+++ src/FriendList.js
@@ -1,18 +1,17 @@
 import { Link } from 'react-router-dom'
-import backButton from './logo1.png'
+import backButton from './back_button.png'
 import './FriendList.css';
 
 function FriendList() {
     return (
+        <div class="friendList-header">
         <div className="friendlist">
-            
             <Link to="/">
-
             <img src={backButton}></img>
-            
-
             </Link>
         </div>
+        </div>
+        
     )
 }
 

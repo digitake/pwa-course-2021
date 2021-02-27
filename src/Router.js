a2914bc08176e4d353cdbc9771baf2aa85bebcfb
boonywdee_sakulchaisiriwit <boonywdee.saku@bumail.net>
2021-02-27 10:36:36 +0700

    เชื่อม Route ไปที่ FriendList ได้แล้ว

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,16 @@
 /** ดึงส่วนประกอบที่จำเป็นมาใช้งานเพื่อสร้าง Component สำหรับ Router*/
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './App';
+import FriendList from './FriendList';
 
 function Router(){
 
     return(
         <BrowserRouter>
            <Switch>
+             <Route path="/friend-list">
+                 <FriendList />
+             </Route>    
              <Route path="/">
                 <App />
              </Route>

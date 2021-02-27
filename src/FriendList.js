77d50307b203e966538a77a83b728e8a8aeb96f9
Noraphat_Thodsadee <noraphat.thod@bumail.net>
2021-02-27 11:28:41 +0700

    ..

---
+++ src/FriendList.js
@@ -0,0 +1,14 @@
+import {Link} from 'react-router-dom';
+
+function FriendList() {
+    return (
+        <div>
+            Friend list
+            <Link to ="/">
+                <img src = "lel.jpg"/>
+            </Link>
+            </div>
+    );
+};
+
+export default FriendList;

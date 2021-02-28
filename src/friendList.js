bb98733d8feeed74dcc9e93e60348c218e3cb7bf
gaharax <anucha.yutt@bumail.net>
2021-02-28 16:01:28 +0700

    Fix Error and Arrange Interface

--- src/friendList.js
+++ src/friendList.js
@@ -2,25 +2,34 @@ import './App.css';
 import{Link} from 'react-router-dom'
 function  FriendList() {
     return(
-        <div className="App-chatroom">
-        <div className="App-header">
-          My Friend List :
-        </div>
-        <div className="App-chatroom-text">
-          Lion 
-        </div>
-        <div className="App-chatroom-text">
-          Cat 
-        </div>
-        <div className="App-chatroom-text">
-          Fisher Man 
-        </div>
-        <Link to="/">
-            <button>
-                Go to Chatroom
-                </button>
-            </Link>
-        </div>
+      <div className="Friend">
+          <div className="Friend-header">
+             My Friend List
+          </div>
+          <div className="Friend-chatroom">
+                <div className="Friend-chatroom-text">
+                  VIP Live
+                </div>
+                <div className="Friend-chatroom-text">
+                  Lion 
+                </div>
+                <div className="Friend-chatroom-text">
+                  Cat 
+                </div>
+                <div className="Friend-chatroom-text">
+                  Fisher Man 
+                </div>
+                <Link to="/">
+                  <button>
+                    Go to Chatroom
+                  </button>
+                </Link>
+          </div>
+      </div>
+        
+          
+        
+        
     )
     
 };

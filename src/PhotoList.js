d529057674f92b21ad515622cea0acc5ee46902c
gaharax <anucha.yutt@bumail.net>
2021-03-03 02:02:20 +0700

    Fix Photolist

--- src/PhotoList.js
+++ src/PhotoList.js
@@ -7,43 +7,43 @@ function  PhotoList() {
             <div class="Text">My Gallery</div>
           <div class="gallery">
               <a target="_blank" href="cat-2.jpg">
-                  <img src="cat-2.jpg" alt="Mountains" width="600" height="400"></img>
+                  <img src="cat-2.jpg" alt="My favorite cat 1" width="600" height="400"></img>
               </a>
               <div class="desc">My favorite cat 1</div>
           </div>
           <div class="gallery">
                 <a target="_blank" href="111.png">
-                    <img src="111.png" alt="Mountains" width="600" height="400"></img>
+                    <img src="111.png" alt="My Cartoon art" width="600" height="400"></img>
                 </a>
                 <div class="desc">My Cartoon art</div>
           </div>
           <div class="gallery">
                 <a target="_blank" href="cat-3.jpg">
-                    <img src="cat-3.jpg" alt="Mountains" width="600" height="400"></img>
+                    <img src="cat-3.jpg" alt="My favorite cat 2" width="600" height="400"></img>
                 </a>
                 <div class="desc">My favorite cat 2</div>
           </div>
           <div class="gallery">
                 <a target="_blank" href="chatroom 3b by first.jpg">
-                    <img src="chatroom 3b by first.jpg" alt="Mountains" width="600" height="400"></img>
+                    <img src="chatroom 3b by first.jpg" alt="Work room interface" width="600" height="400"></img>
                 </a>
                 <div class="desc">Work room interface</div>
           </div>
           <div class="gallery">
                 <a target="_blank" href="funny-cat.jpeg">
-                    <img src="funny-cat.jpeg" alt="Mountains" width="600" height="400"></img>
+                    <img src="funny-cat.jpeg" alt="My favorite cat 3" width="600" height="400"></img>
                 </a>
                 <div class="desc">My favorite cat 3</div>
           </div>
           <div class="gallery">
                 <a target="_blank" href="interface design by cha.jpg">
-                    <img src="interface design by cha.jpg" alt="Mountains" width="600" height="400"></img>
+                    <img src="interface design by cha.jpg" alt="Chat room interface" width="600" height="400"></img>
                 </a>
                 <div class="desc">Chat room interface</div>
           </div>
           <div class="gallery">    
                 <a target="_blank" href=" mobile_app_presenting.jpg">
-                    <img src=" mobile_app_presenting.jpg" alt="Mountains" width="600" height="400"></img>
+                    <img src=" mobile_app_presenting.jpg" alt="Present interface" width="600" height="400"></img>
                 </a>
                  <div class="desc">Present interface</div>
           </div>

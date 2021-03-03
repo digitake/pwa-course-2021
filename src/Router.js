2df373a730071f7226347e0dd10382b3869a6bf9
Khanin_Jittapan <Khanin.jitt@bumail.net>
2021-03-03 15:38:35 +0700

    Update : Router.js

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,16 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import App from './App'
 import FriendList from './FriendList'
+import Loading from './Loading'
 
 function Router() {
 
     return(
         <BrowserRouter>
             <Switch>
+                <Route path="/loading">
+                    <Loading />
+                </Route>
                 <Route path="/friend-list">
                     <FriendList />
                 </Route>

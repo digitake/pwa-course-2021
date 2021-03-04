1ede27e2040769f5715bdde149fb07194740353b
Ratpakorn <ratpakorn.sunt@bumail.net>
2021-03-04 19:32:43 +0700

    แก้ ล่าสุด

--- src/Router.js
+++ src/Router.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-import {BrowserRouter,Switch,Route} from 'react-router-dom';
-import App from './App';
-import FriendsList from './FriendsList';
-
-function Router() {
-    return (
-    <BrowserRouter>
-     <Switch>
-     <Route path="/friend-list">
-      <FriendsList />
-     </Route>
-     <Route path="/">
-     <App/>
-     </Route>
-     </Switch>
-    </BrowserRouter>
-    )
-=======
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import App from "./App";
 import FriendList from "./FriendList";
@@ -35,7 +16,6 @@ function Router(){
             </Switch>
         </BrowserRouter>
     );
->>>>>>> 933622d32c68d01635d6dcaff33149ef58308701
 };
 
 export default Router;

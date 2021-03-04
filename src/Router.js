97889aa724ee8f7a8a7d08382be0858faa787fb2
natchaphon_sirisangsawang <natchaphon.siri@bumail.net>
2021-03-05 00:31:55 +0700

    My Work : +2 AppPages, แก้ manifest.json, เเก้ไขหน้าAppอันเก่า && wait for my team pull

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,24 @@
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import App from "./App";
 import FriendList from "./FriendList";
+import EnterApp_3ofghost from "./EnterApp_3ofghost";
+import Eye_LoadingScene from "./Eye_LodeingScene";
 
 function Router(){
     
     return(
         <BrowserRouter>
             <Switch>
+                <Route path="/Eye_Scene">
+                    <Eye_LoadingScene />
+                </Route>
                 <Route path="/friend-list">
                     <FriendList />
                 </Route>
-                <Route pat="/">
+                <Route path="/EnterApp_3ofghost">
+                    <EnterApp_3ofghost />
+                </Route>
+                <Route path="/">
                     <App/>
                 </Route>
             </Switch>

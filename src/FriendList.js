933622d32c68d01635d6dcaff33149ef58308701
natchaphon_sirisangsawang <natchaphon.siri@bumail.net>
2021-03-04 19:23:25 +0700

    Prepare to groupWork

--- src/FriendList.js
+++ src/FriendList.js
@@ -1,8 +1,16 @@
+import {Link} from "react-router-dom";
 
 function FriendList(){
     return(
         <div>
             Friend List
+            <Link to="/">
+                <button>
+                    Go to Main
+                </button>
+                
+                <img src="pruekProfile.jpg"/>
+            </Link>
         </div>
     );
 };

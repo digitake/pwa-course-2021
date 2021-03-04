5abd6e5f832f488c7b204389c1a6c2608ac4ad55
TeerathepS <teerathep.saia@bumail.net>
2021-03-05 02:03:25 +0700

    ทำปุ่ม

--- src/LobbySetting.js
+++ src/LobbySetting.js
@@ -43,6 +43,21 @@ function LobbySetting() {
                         </div>
                     })
                 }
+                <div className="Private">
+                    Private Room
+                </div>
+                <label className="switch">
+                    <input type="checkbox"/>
+                    <span className="slider round">
+                    </span>
+                </label>
+
+                <label className="switch">
+                    <input type="checkbox"/>
+                    <span className="slider round">
+                    </span>
+                </label>
+
                 <label className="switch">
                     <input type="checkbox"/>
                     <span className="slider round">

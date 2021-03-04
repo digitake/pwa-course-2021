bea690ef549b7e6de0262b1794b6fcc94c18d647
TeerathepS <teerathep.saia@bumail.net>
2021-03-05 02:33:24 +0700

    ทำปุ่มทั้งหมดและเพิ่มคำอธิบาย

--- src/LobbySetting.js
+++ src/LobbySetting.js
@@ -44,8 +44,18 @@ function LobbySetting() {
                     })
                 }
                 <div className="Private">
-                    Private Room
+                    <h1>Private Room</h1>
                 </div>
+                <div className="AllowHint">
+                    <h1>AllowHint</h1>
+                </div>
+                <div className="AllowJoin">
+                    <h1>Allow to Join between game</h1>
+                </div>
+                <div className="PlayerName">
+                    <h1>Show player's name</h1>
+                </div>
+                
                 <label className="switch">
                     <input type="checkbox"/>
                     <span className="slider round">

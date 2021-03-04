c00256fdd4ad8832679cdb0497f413edd900bd61
Kanpicha10 <Kanpicha.panp@bumail.net>
2021-03-05 00:40:13 +0700

    เชื่อม Route ไป FriendList

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,15 @@
 import{ BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './App';
+import FriendList from './FriendList';
 
 function Router() {
 
     return (
         <BrowserRouter>
         <Switch>
+            <Route path="/friend-List">
+                <FriendList />
+                </Route>
             <Route path="/">
              <App />
             </Route>

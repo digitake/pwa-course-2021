520e198e0ef1f19d6f5bf93a5987cb2de0d2df64
boonywdee_sakulchaisiriwit <boonywdee.saku@bumail.net>
2021-03-05 00:28:16 +0700

    test3

---
+++ src/FriendList.js
@@ -0,0 +1,16 @@
+import { Link} from 'react-router-dom'
+
+function FriendList() {
+    return(
+        <div>
+            Friend List
+            <Link to="/">
+                <button>
+                    Go Chatroom
+                </button>
+            </Link>
+        </div>
+    )
+};
+
+export default FriendList;

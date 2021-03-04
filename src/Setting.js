1ae2a2e84ff6915f98dcf0e4b4222bdd9f1e5ae6
ThanatipLnwza <Thanatip.ngan@bumail.net>
2021-03-04 17:22:37 +0700

    add header

--- src/Setting.js
+++ src/Setting.js
@@ -3,12 +3,15 @@ import { Link } from 'react-router-dom';
 function Settings(){
     return(
         <div className="contain">
-            <div className="contents">
-            <Link to="/">
-            <button>
-            PrivateChatRoom
-            </button>
-            </Link>
+            <div className="App-head">
+                Settings
+            </div>
+        <div className="contents">
+        <Link to="/">
+        <button>
+        PrivateChatRoom
+        </button>
+        </Link>
             </div>
         </div>
     );

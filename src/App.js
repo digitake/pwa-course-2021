f1c5e4b3317c51624adca27b0a87fe7f783a84c5
toke6310 <77868843+toke6310@users.noreply.github.com>
2021-03-05 00:11:46 +0700

    Revert "ขอมาทำ UI Chi CHi"

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import {useState} from 'react';
+import { Link} from 'react-router-dom'
 
 
 function App() {
@@ -20,17 +22,16 @@ function App() {
     }
   };
 
-
   return (
     <div className="App">
       <div className="App-header">
-
-      <div className="App-header1" onClick={onSend}></div>
-      <div className="App-header2" onClick={onSend}></div>
-      <div className="App-header3" onClick={onSend}></div>  
-
+        Miyawaki Sakura
+        <Link to="/friend-list">
+          <button>
+            Go Test
+          </button>
+        </Link>
       </div>
-      
       <div className="App-chatroom">
         {
           lines.map(x => {
@@ -43,11 +44,8 @@ function App() {
       <div className="App-textbox">
         <input type="text" className="App-textbox-input" 
         value={text} onChange={onTextChange} onKeyPress={keyPress}/>
-
-        <div className="App-textbox-send" onClick={onSend}></div>
-        <div className="App-textbox-send1" onClick={onSend}></div>
-        <div className="App-textbox-send2" onClick={onSend}></div>
         
+        <div className="App-textbox-send" onClick={onSend}>Send!</div>
       </div>
     </div>
   );
--- src/Router.js
+++ src/Router.js
@@ -1,18 +1,19 @@
+/** ดึงส่วนประกอบที่จำเป็นมาใช้งานเพื่อสร้าง Component สำหรับ Router*/
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './App';
 import FriendList from './FriendList';
-/** FriendList*/
-function Router() {
 
-    return (
+function Router(){
+
+    return(
         <BrowserRouter>
-            <Switch>
-                <Route path="/friend-List">
-                    <FriendList />
-                </Route>
-                <Route path="/">
-                    <App/>
-                </Route>
+           <Switch>
+             <Route path="/friend-list">
+                 <FriendList />
+             </Route>    
+             <Route path="/">
+                <App />
+             </Route>
             </Switch>
         </BrowserRouter>
     )

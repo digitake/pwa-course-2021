556670bd2dbf97b3e22e885d66cde023b357e78d
ThanatipLnwza <Thanatip.ngan@bumail.net>
2021-03-04 22:19:08 +0700

    add social icon

--- src/Profiles.js
+++ src/Profiles.js
@@ -1,1 +1,35 @@
 import './Profiles.css';
+
+
+function profiles() {
+    
+    return (
+<div className="Header">
+            <div className="Container">
+                Profiles
+               <ul className="Function">
+                   <li className="Function-link">
+                   <a href="#"><FiBell/>Facebook</a>
+                   </li>
+                   <li className="Function-link">
+                   <a href="#"><BiPaintRoll/>Instagram</a>
+                   </li>
+                   <li className="Function-link">
+                   <a href="#"><BiPalette/>Spotify</a>
+                   </li>
+                   <li className="Function-link">
+                   <a href="#"><FiInfo/>Line</a>
+                   </li>
+                   </ul> 
+        <div className="App-contents">
+        </div>
+        <Link to="/PrivateChat">
+        <button>
+        PrivateChatRoom
+        </button>
+        </Link>
+            </div>
+        </div>
+    );
+};
+export default Profiles;

11a4a4457954c0c6cd94732a366b5baed8eb9c4a
lapassuth <lapas.suth@bumail.net>
2021-03-05 17:11:53 +0700

    แก้ไขหน้าโฮมครั้งที่ 2

--- src/Home.js
+++ src/Home.js
@@ -6,31 +6,34 @@ function  Home() {
       <div class="background">
         <div class="text">
             Welcome
-            <div class ="link">
+      </div>
+      <div class ="link">
             <Link to="/friend-list">
             <button>
              Go to Friend-List
              </button>
             </Link>
+            </div>
           <Link to="/chat-room">
             <button>
              Go to Chat Room
              </button>
           </Link>
+          <div class="link">
           <Link to="/doc-work">
             <button>
              Go to Document Work
              </button>
           </Link>
+          </div>
+          <div class="link">
           <Link to="/photo-list">
             <button>
              Go to Photo-List
              </button>
              </Link>
+             </div>
       </div>
-        </div>
-        
-        </div>
 
     )
     

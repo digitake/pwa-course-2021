f1baf72758cb98874077261834797c1292a26a66
lapassuth <lapas.suth@bumail.net>
2021-03-05 16:52:29 +0700

    อัพเดทหน้าโฮม

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,37 @@
-import './App.css';
+
 import{Link} from 'react-router-dom'
+import './Home.css';
 function  Home() {
     return(
-      <div>
-          Home
-          <Link to="/cradit">
+      <div class="background">
+        <div class="text">
+            Welcome
+            <div class ="link">
+            <Link to="/friend-list">
             <button>
-             Go to Cradit-List
+             Go to Friend-List
              </button>
             </Link>
+          <Link to="/chat-room">
+            <button>
+             Go to Chat Room
+             </button>
+          </Link>
+          <Link to="/doc-work">
+            <button>
+             Go to Document Work
+             </button>
+          </Link>
+          <Link to="/photo-list">
+            <button>
+             Go to Photo-List
+             </button>
+             </Link>
+      </div>
         </div>
+        
+        </div>
+
     )
     
 };

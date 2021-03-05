96bead05b06dbb66279231decc2a7e8e73724eb8
maguro-min <b7c3yq@gmail.com>
2021-03-05 21:36:18 +0700

    Add FriendList Page

--- src/FriendList.js
+++ src/FriendList.js
@@ -1,16 +1,28 @@
+import { useState } from 'react';
+import './FriendList.css';
 import { Link } from 'react-router-dom';
 
 function FriendList() {
-    return (
-        <div>
-            FriendList
-            <Link to="/">
+  return (
+    <div className="FriendList">
+      <div className="FriendList-header">
+      contacts
+      <div className="FriendList-Headers">
+      <Link to="/">
                 <botton>
-                    Go to Chatroom
+                    Go to chatroom
                 </botton>
             </Link>
-        </div>
-    )
+      </div>
+      </div>
+      <div className= "FriendList-Contact-info">
+      </div>
+      <div className="FriendList-chatroom">
+        {
+        }
+      </div>
+    </div>
+  );
 };
 
 export default FriendList;

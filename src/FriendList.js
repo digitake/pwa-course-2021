83a6fcf61fec98eb358801473e37f60221ad72d1
kennykung2 <thanakrit.siri@bumail.net>
2021-03-05 23:15:45 +0700

    update ewallet page

--- src/FriendList.js
+++ src/FriendList.js
@@ -1,14 +1,63 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import './FriendList.css';
 
-function FriendList(){
-    return(
-        <div>
-            FriendList
-            <Link to="/">
-                <img src= "logo.svg"/>
-            </Link>
+
+function App() {
+  const [text, setText] = useState("");
+  const [lines, setLines] = useState([]);
+  
+  const onTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const onSend = () =>{
+    setLines([...lines, text]);
+    setText("");
+  };
+
+  const keyPress = (event) => {
+    if (event.which === 13){
+      onSend();
+    }
+  };
+  const img1 = require('./design/balance.jpg')
+
+
+  return (
+    <div className="App">
+      <div className="App-header">
+
+      <div className="App-header1" onClick={onSend}></div>
+      <div className="App-header2" onClick={onSend}></div>
+      <div className="App-header3" onClick={onSend}></div>  
+
+      </div>
+      
+      <div className="App-bg">
+       <div className="App-chatroom-text" onClick={onSend}> </div>
+          
+        
+      </div>
+
+
+      <div className="App-textbox">
+        
+       
+
+        <div className="App-textbox-send" onClick={onSend}></div>
+        <div className="App-textbox-send1" onClick={onSend}></div>
+        <div className="App-textbox-send2" onClick={onSend}></div>
+        
+
+      </div>
+      <div className="Menu-1">
+        <img src={img1}/>
         </div>
-    );
-};
+    </div>
+  );
+}
 
-export default FriendList;
+
+
+export default App;

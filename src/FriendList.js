ef8ac2b148def4946f5bb36eb4a4aa99c9f6f679
NitithornRm <nitithorn.rutm@bumail.net>
2021-03-05 22:41:34 +0700

    merge guy

--- src/FriendList.js
+++ src/FriendList.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-import { Link } from 'react-router-dom'
-import backButton from './back_button.png'
-import './FriendList.css';
-
-function FriendList() {
-return (
-
-    <div className="friendList">
-       <div class="friendList-header">
-           <div class="friendList-center">
-               <div class="friendList-friend">
-                <div class="item1">User.1</div>
-                <div class="item2">User.2</div>
-                <div class="item3">User.3</div>
-                <div class="item4">User.4</div>
-                <div class="item4">User.5</div>
-                <div class="item4">User.6</div>
-                <div class="item4">User.7</div>
-               </div>
-           </div>
-           <div className="friendlist">
-               <Link to="/">
-                <img src={backButton}></img>
-              </Link>
-            </div>
-        </div>
-    </div>
-        
-    )
-}
-=======
 import { Link } from 'react-router-dom';
 
 function FriendList(){
@@ -44,6 +12,5 @@ function FriendList(){
         </div>
     );
 };
->>>>>>> c4a1fcf51668531f6a4a8d9f98fbd74c22aa5355
 
 export default FriendList;
--- src/Router.js
+++ src/Router.js
@@ -1,31 +1,11 @@
-<<<<<<< HEAD
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import App from './App';
-import FriendList from './FriendList';
-import Shop from "./Shop";
-=======
 import{ BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './App';
 import FriendList from './FriendList';
->>>>>>> c4a1fcf51668531f6a4a8d9f98fbd74c22aa5355
 
 function Router() {
 
     return (
         <BrowserRouter>
-<<<<<<< HEAD
-            <Switch>
-                <Route path="/friend-list">
-                    <FriendList />    
-                </Route>
-                <Route path="/shop">
-                    <Shop />
-                </Route>  
-                <Route path="/">
-                    <App />
-                </Route>
-            </Switch> 
-=======
         <Switch>
             <Route path="/friend-List">
                 <FriendList />
@@ -34,13 +14,8 @@ function Router() {
              <App />
             </Route>
         </Switch>
->>>>>>> c4a1fcf51668531f6a4a8d9f98fbd74c22aa5355
         </BrowserRouter>
     )
 };
 
-<<<<<<< HEAD
-
-=======
->>>>>>> c4a1fcf51668531f6a4a8d9f98fbd74c22aa5355
 export default Router;

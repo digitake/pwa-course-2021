5400dd9db74c32d0cab2b347e65bbd3047b77415
Noraphat_Thodsadee <noraphat.thod@bumail.net>
2021-03-05 23:21:00 +0700

    Profile อัพเดต

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -1,16 +1,56 @@
-
+import './App.css';
 import { Link } from "react-router-dom";
 
 function ProfilePage(){
     return(
-        <div>
-            Profile Page
-            <Link to="/">
-              <button>
-                  Back
-               </button>
-            </Link>
+    <div className="ProfilePage">
+
+        <div className="Profile-background">
+      
         </div>
+
+        <div className="Profile-coverPhoto">
+            <div className="Profile-Profilephoto">
+      
+            </div>
+        </div>
+
+        <div className="Profile-header">
+            <div className="Profile-room-Button">
+             <Link to = "/">
+               <image>
+               <img src ="buttonChat.png"/>
+               </image>
+             </Link>
+
+             <Link to = "/friend-list">
+               <image>
+               <img src ="buttonFriend.png"/>  
+               </image>
+             </Link>
+
+             <Link to = "/shop-page">
+               <image>
+               <img src ="buttonShop.png"/>  
+               </image>
+             </Link>
+
+             <Link to = "/game-page">
+               <image>
+               <img src ="buttonGame.png"/> 
+               </image>
+             </Link>
+            </div>
+
+        </div>
+
+        <div className="Profile-chatroom">
+           Name Surname
+        </div>
+
+           
+
+    </div>
 
     );
 };

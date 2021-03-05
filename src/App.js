8a342f3d23335ac5805016bcd9e5cc8501973dc3
harit_tangoon <harit.tang@bumail.net>
2021-03-05 21:05:18 +0700

    TEST

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-
+import { Link} from 'react-router-dom'
 
 function App() {
   const [text, setText] = useState("");
--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './App';
 import FriendList from './FriendList';
+
 /** FriendList*/
 function Router() {
 

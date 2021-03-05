2ab20581e8480ba2eb8e1043eaf8d1115bb38989
ElijaChinda <Elija.chin@bumail.net>
2021-03-05 22:38:55 +0700

    FriendList.CSS

--- src/FriendList.js
+++ src/FriendList.js
@@ -1,23 +1,35 @@
+import './FriendList.css';
 import { Link } from 'react-router-dom';
 
 function FriendList() {
     
     return (
-        <div>
-            Friend List
-            <Link to="/">
-                <button>
-                    Go Chatroom
-                </button>
-            </Link>
+       <div className = "FriendList">
+             <div className = "FriendList-header">
+             </div>
+        <div className = "FriendList-chatroom">
+        <div className = "FriendList-Main">
             <div>
+                Friend List
+                <Link to="/">
+                    <button>
+                        Go Chatroom
+                     </button>
+                 </Link>
+                <div>
                 <Link to="/Profile">
                     <button>
                         Go Profile
                     </button>
                 </Link>
+                </div>
             </div>
         </div>
+        </div>
+        </div>
+
+
+
 
     );
 };

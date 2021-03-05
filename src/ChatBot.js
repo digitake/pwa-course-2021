3ff0ed5b55c762fb896df28744e2789a1bc540c2
Arunsawad_Boonthus <Arunsawad.boon@bumail.net>
2021-03-05 14:24:08 +0700

    เพิ่ม import หน้า bot&friend

--- src/ChatBot.js
+++ src/ChatBot.js
@@ -1,3 +1,7 @@
+import './App.css';
+import {useState} from 'react';
+import { Link } from 'react-router-dom';
+
 function ChatBot() {
     return (
     <div>
--- src/FriendList.js
+++ src/FriendList.js
@@ -1,3 +1,5 @@
+import './App.css';
+import {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 function FriendList() {

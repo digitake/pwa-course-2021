16617576cc33bbf721fad3af1d0fe9477df8f4b0
nottae12 <pannathat.lima@bumail.net>
2021-03-06 17:53:53 +0700

    ืืnew

---
+++ src/Profile.js
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import App from './App';
+import main from './image/BT.png';
+import pro from './123.jpg';
+
+function  Profile() {
+
+    return(
+        <App>
+        <div>
+        <body>
+           <center><img src={pro} width="200" border="5" align="top" /></center>
+                <p align="center">
+                    <h1>
+                    Name
+                    </h1>
+                    <h1>
+                    ID ********
+                    </h1>
+                </p>
+            </body>
+        </div>
+        </App>
+    )
+    
+}
+export default Profile;

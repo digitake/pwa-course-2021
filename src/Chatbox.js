2d3bf14083cca307501f1efc566b60ff99afb977
janjao93 <janjao937@gmail.com>
2021-03-06 15:12:40 +0700

    ย้ายApp.jsเข้าไปในChatbox.jsรวมถึงcss

---
+++ src/Chatbox.js
@@ -0,0 +1,62 @@
+import './Chatbox.css';
+import {useState} from 'react';
+import{Link} from 'react-router-dom';
+
+function Chatbox() {
+  const [text, setText] = useState("");
+  const [lines, setLines] = useState([]);
+
+  const onTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const onSend = () =>{
+    setLines([...lines, text]);
+    setText("");
+  };
+
+  const keyPress = (event) => {
+    if (event.which === 13){
+      onSend();
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="App-header">
+
+
+        App Chatroom(ClassWork)
+        <div className="App-textbox-send">
+          
+            <Link to="/friend-list">
+            <button>
+                Go to Friend list(Class Work)
+            </button>
+            </Link>
+       
+        </div>
+      </div>
+      <div className="App-chatroom">
+        {
+          lines.map(x => {
+            return <div className="App-chatroom-text">
+                    {x}
+                   </div>
+          })
+        }
+      </div>
+      <div className="App-textbox">
+        <input type="text" className="App-textbox-input" 
+        value={text} onChange={onTextChange} onKeyPress={keyPress}/>
+         
+        <div className="App-textbox-send" onClick={onSend}>Send!</div>
+      
+        
+        
+      </div>
+    </div>
+  );
+}
+
+export default Chatbox;

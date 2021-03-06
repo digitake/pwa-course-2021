ac49cdd473fc6b8e7310fc9edcf60450c67d8ae5
janjao93 <janjao937@gmail.com>
2021-03-06 15:53:04 +0700

    ใช้งานlayout component

--- src/Chatbox.js
+++ src/Chatbox.js
@@ -23,20 +23,7 @@ function Chatbox() {
 
   return (
     <div className="App">
-      <div className="App-header">
-
-
-        App Chatroom(ClassWork)
-        <div className="App-textbox-send">
-          
-            <Link to="/friend-list">
-            <button>
-                Go to Friend list(Class Work)
-            </button>
-            </Link>
-       
-        </div>
-      </div>
+      
       <div className="App-chatroom">
         {
           lines.map(x => {

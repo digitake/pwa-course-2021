ac2d1b44c8b1598ed3bee417ee2022a3e1cc1463
adisorn080843 <adisorn.numk@bumail.net>
2021-03-06 12:38:56 +0700

    แก้หน้า login

--- src/project/Prologin.js
+++ src/project/Prologin.js
@@ -1,22 +1,33 @@
 import './Prologin.css';
 import { Link } from 'react-router-dom'
 
-function Prologin() {
+function Prologin({children}) {
   return (
-    <div className="Prologin">
-      <header className="Prologin-header">
-         <img className="Prologin-header-logo" src="/images/boxchat.jpg" />
-      </header>
-      <div className="signin-logo">
-        <Link to="/">
-           <img src= "/images/sign.jpg" alt = "/images/sign.jpg" />      
-        </Link>
+    <div className = "Prologin">
+
+      <div className="header-top">
+        {children}
       </div>
-      <div className="registery-logo">
-        <Link to="/">
-           <img src= "/images/register.jpg" alt = "/images/register.jpg" />      
-        </Link>
-      </div>
+
+      <div className ="background-login">
+         
+             <div to="/" className="Prologin-logo">
+                 <img src= "/images/boxchat.jpg" alt = "/images/boxchat.jpg" 
+               width="370px" height="290px"/> 
+             </div>
+
+             <Link to="/" className="signin-logo">
+                 <img src= "/images/sign.jpg" alt = "/images/sign.jpg"
+               width="320px" height="85px"/> 
+             </Link>
+
+             <Link to="/" className="register-logo">
+                 <img src= "/images/register.jpg" alt = "/images/register.jpg" 
+               width="240px" height="70px"/> 
+             </Link>
+
+      </div>   
+
     </div>
   );
 }

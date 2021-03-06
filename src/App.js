82a8e7fff42fd093554382eddfad6491d0350b68
newpuwinwin <puwin.meep@bumail.net>
2021-03-06 15:53:11 +0700

    ใช้งาน layout component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
 import './App.css';
-import {useState} from 'react';
-import{Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import Chatbox from './Chatbox';
 
 function App() {
-  const [text, setText] = useState("");
-  const [lines, setLines] = useState([]);
-
-  const onTextChange = (event) => {
-    setText(event.target.value);
-  };
-
-  const onSend = () =>{
-    setLines([...lines, text]);
-    setText("");
-  };
-
-  const keyPress = (event) => {
-    if (event.which === 13){
-      onSend();
-    }
-  };
+  
 
   return (
     <div className="App">
@@ -36,23 +20,10 @@ function App() {
             </Link>
        
         </div>
+      <div className="App-content">
+       <Chatbox />
       </div>
-      <div className="App-chatroom">
-        {
-          lines.map(x => {
-            return <div className="App-chatroom-text">
-                    {x}
-                   </div>
-          })
-        }
-      </div>
-      <div className="App-textbox">
-        <input type="text" className="App-textbox-input" 
-        value={text} onChange={onTextChange} onKeyPress={keyPress}/>
-         
-        <div className="App-textbox-send" onClick={onSend}>Send!</div>
-      
-        
+             
         
       </div>
     </div>

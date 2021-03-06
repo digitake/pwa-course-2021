436144c9fe73d466f41227359dea94bd361efc4d
Wisarut_Payonpaiboonsuk <wisarut.payo@bumail.net>
2021-03-06 11:19:02 +0700

    week 7

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
 import './App.css';
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-function App() {
 
-    const [text,setText] = useState("");
-
-    const [lines,setLines] = useState([]);
-
-    const onTextChange = (event) => {
-      setText(event.target.value);
-    };
-
-    const onSend =() => {
-      setLines(lines => [...lines,text]);
-      setText("");
-    };
-
-    const keyPress = (event) => {
-      if (event.which === 13){
-        onSend();
-      }
-    };
+function App({children}) {
 
   return (
     <div className="App">
@@ -32,21 +13,9 @@ function App() {
                   </button>
               </Link>
         </div>
-      <div className="App-chatroom">
-        {
-          lines.map(x =>{
-           return <div className="App-chatroom-text">
-                      {x}
-                  </div>
-          })
-        } 
-      </div>
-      <div className="App-textbox">
-      <input type="text" className="App-textbox-input" 
-        value={text} onChange={onTextChange} onKeyPress={keyPress}/>
-
-        <div className="App-textbox-send" onClick={onSend}>Send!</div>
-      </div>
+        <div className="App-content">
+          {children}
+        </div>
     </div>
   );
 }
--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,8 @@
 import {BrowserRouter, Switch , Route} from 'react-router-dom';
-/*import App from './App';*/
+import Chatbox from './Chatbox';
 import ChatRoom from './ChatRoom';
 import ProFriend from './project/ProFriend';
+import FriendList from './FriendList';
 
 
 function Router() {
@@ -12,8 +13,14 @@ function Router() {
                 <Route path="/chatroom">
                     <ChatRoom />
                 </Route>
+                <Route path="/fri">
+                    <FriendList />
+                </Route>
                 <Route path="/ProFriend">
                     <ProFriend />
+                </Route>
+                <Route path="/">
+                    <Chatbox />
                 </Route>
             </Switch>
         </BrowserRouter>

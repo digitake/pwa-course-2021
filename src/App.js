7522372078971ed7a6bff3af62deadc52cc6dfc7
maguro-min <b7c3yq@gmail.com>
2021-03-06 15:45:26 +0700

    Fix error path

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import './App.css';
 import {Link} from 'react-router-dom';
-import { useState } from 'react';
+import Chatbox from './Chatbox';
 
 function App() {
-  const [text, setText] = useState("");
-  const [lines, setLines] = useState([]);
-  const onTextChange = (event) =>{
-    setText(event.target.value);
-  };
-  const onSend = () =>{
-    setLines([...lines, text]);
-    setText("");
-  };
-  const keyPress = (event) => {
-
-    if (event.which === 13){
-      onSend();
-    }
-  }
   return (
     <div className="App">
       <div className="App-header">
@@ -27,25 +12,9 @@ function App() {
                 </botton>
             </Link>
       </div>
-      <div className="App-chatroom">
-        {
-        lines.map(x => {
-          return <div className="App-chatroom-text">
-                {x}
-                </div>
-          })
-        }
-        <Link to ="/PageDrawing">
-          <botton>
-            Go to PageDrawing
-          </botton>
-        </Link>
-      </div>
-      <div className="App-textbox">
-        <input type="text" className="App-textbox-input" 
-        value={text} onChange={onTextChange}onKeyPress={keyPress}/>
-        <div className="App-textbox-send" onClick={onSend}>Send!</div>
-      </div>
+    <div className="App-Content">
+    <Chatbox />
+    </div>
     </div>
   );
 }

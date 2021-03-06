c68bc8ea2c947ff93379e1ed4a315691ce673c39
bird02648 <birdpb02648@gmail.com>
2021-03-06 15:12:45 +0700

    ย้ายApp.js เข้าไปไว้ในChatbox.jsแทน

---
+++ src/Chatbox.js
@@ -0,0 +1,50 @@
+  
+import './Chatbox.css';
+
+import {useState} from 'react' ;
+
+function Chatbox() {
+
+  const [text,setText] = useState("");const [lines, setLines] = useState([]);
+  const  onTextChange = (event) =>
+  {
+    setText(event.target.value);
+  };
+
+  const onSend = ()=>{
+    setLines([...lines,text]);
+    setText("");
+  };
+  const keyPress = (event)=>{
+
+    if (event.which === 13){
+      onSend();
+    }
+  };
+
+
+  return (
+    <div className="App">
+      <div className="App-header">
+      </div>
+      <div className="App-chatroom">
+        {
+
+          lines.map(x =>{
+            return <div className="App-chatroom-text">
+              {x}
+              </div>
+          })
+        }
+
+      </div>
+      <div className="App-textbox">
+        <input type="text" className="App-textbox-input" value={text} onChange={onTextChange} onKeyPress={keyPress}/>
+        <div className="App-textbox-send" onClick={onSend} >ส่ง</div>
+
+      </div>
+    </div>
+  );
+}
+
+export default Chatbox;

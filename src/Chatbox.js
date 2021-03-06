1d2ee90b7a89f16e4a5e94385f43f3cfcb053c81
settakorn_sris <settakorn.sris@bumail.net>
2021-03-06 15:12:31 +0700

    Move App.js into Chatbox.js with.css

---
+++ src/Chatbox.js
@@ -0,0 +1,67 @@
+import { Link } from 'react-router-dom';
+import './Chatbox.css';
+import { useState } from 'react';
+import LinkTo from './Components/LinkManager';
+
+function Chatbox() {
+
+  const [text, setText] = useState('');
+  const [lines, setLines] = useState([]);
+
+  const onSend = () => {
+    if (text.length < 1) return;
+    setLines([...lines, text]);
+    setText("");
+  }
+
+  const onTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const onKeyPress = (event) => {
+    if (event.which === 13) {
+      onSend();
+    }
+  }
+
+  return (
+    <div className="App">
+      <div className="App-navigator">
+        <LinkTo value='Welcome' to='/loading' />
+        <LinkTo value='Insert Password' to='/insert-password' />
+        <LinkTo value='Register' to='/register' />
+        <LinkTo value='Login' to='/login' />
+        <LinkTo value='Feed' to='/feed' />
+        <LinkTo value='Settings' to='/settings' />
+        <LinkTo value='Profile Setting' to='/profile-setting' />
+        <LinkTo value='Insepect Profile' to='/insepect-profile' />
+      </div>
+      <div className="App-header">
+        Neptune Chat
+      </div>
+      <div className="App-chatroom">
+        {
+          lines.map(msg => {
+            return <div key={msg} className="App-chatroom-text">
+              {msg}
+            </div>
+          })
+        }
+
+      </div>
+      <div className="App-textbox">
+        <input placeholder='Type something...' type="text" className="App-textbox-input" value={text} onKeyPress={onKeyPress} onChange={onTextChange} />
+        <div className="App-textbox-send" onClick={onSend}>Send!</div>
+      </div>
+      <div>
+        <Link to="/Register">
+          <button>
+            Go to Register
+        </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Chatbox;

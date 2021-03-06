e6cd544603e2ad7facfad246823e96f80dae82be
bird02648 <birdpb02648@gmail.com>
2021-03-06 15:30:34 +0700

    แก้ไข App.js ให้เป็นพื้นที่ว่าง

--- src/App.js
+++ src/App.js
@@ -1,47 +1,16 @@
   
 import './App.css';
-
+import Chatbox from './Chatbox'
 import {useState} from 'react' ;
 
-function App() {
-
-  const [text,setText] = useState("");const [lines, setLines] = useState([]);
-  const  onTextChange = (event) =>
-  {
-    setText(event.target.value);
-  };
-
-  const onSend = ()=>{
-    setLines([...lines,text]);
-    setText("");
-  };
-  const keyPress = (event)=>{
-
-    if (event.which === 13){
-      onSend();
-    }
-  };
-
-
+function App() 
+{
   return (
     <div className="App">
       <div className="App-header">
       </div>
-      <div className="App-chatroom">
-        {
-
-          lines.map(x =>{
-            return <div className="App-chatroom-text">
-              {x}
-              </div>
-          })
-        }
-
-      </div>
-      <div className="App-textbox">
-        <input type="text" className="App-textbox-input" value={text} onChange={onTextChange} onKeyPress={keyPress}/>
-        <div className="App-textbox-send" onClick={onSend} >ส่ง</div>
-
+      <div className="App-content">
+        <Chatbox />
       </div>
     </div>
   );

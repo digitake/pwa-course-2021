52c0028f1ada9bc70209d6934184f09c84c3f001
Nattakan-phim <Nattakan.phim@gmail.net>
2021-03-06 13:11:28 +0700

    Update code && edit

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,29 +1,33 @@
 import {Link} from 'react-router-dom'
+import './App.css';
 
-
-function HomePage() {
+function HomePage({children}) {
   return(
 
      <div className = "HomePageInfo">
+
         <div className = "ChatBar">
             <Link to = "/Chat-Room">
-            <img src = "ChatBoxBar.Png" alt = "" />
+                <img src = "ChatBoxBar.Png" alt = "" />
             </Link>
         </div>
         <div className = "GroupBar">
             <Link to = "/GroupChatBox">
-            <img src = "GroupChat.Png" alt = "" />
+                <img src = "GroupChat.Png" alt = "" />
             </Link>
         </div>
         <div className = "PrivateBar">
             <Link to = "/PrivateChatBox">
-            <img src = "Private.Png" alt = "" />
+                <img src = "Private.Png" alt = "" />
             </Link>
         </div>
         <div className = "SettingBar">
             <Link to = "/SettingPage">
-            <img src = "SettingBar.Png" alt = "" />
+                <img src = "SettingBar.Png" alt = "" />
             </Link>
+        </div>
+        <div className ="MenuHomeBar">
+            {children}
         </div>
 
     </div>
--- src/RegisterNextStep.js
+++ src/RegisterNextStep.js
@@ -1,5 +1,5 @@
 import {Link} from 'react-router-dom'
-
+import './App.css';
 
 function RegisterNextStep() {
     return(

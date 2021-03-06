6db7c70a8383129f917b1465247ae42a96f4cb59
Assadawut_Hanlakorn <assadawut.hanl@bumail.net>
2021-03-06 09:46:39 +0700

    Change './app.css' to './Chatbox.css'

--- src/Chatbox.js
+++ src/Chatbox.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './Chatbox.css';
 import{useState}from 'react'
 import {Link} from 'react-router-dom';
 
@@ -41,16 +41,6 @@ const onSend = (event) =>{
                </button>
              </Link>
       </div> 
-      <div className="App-header App-logo ">
-        <div className="Friend-Button Menu-Button">
-             <Link to = "/friend-list">
-              <img src= "Back-32.png"/>
-             </Link>
-             <Link to ="/menu">
-             <img src= "down32.png" align ="right"/> 
-             </Link>
-        </div>
-      </div>
       <div className="App-chatroom">
         {
           lines.map(x =>{

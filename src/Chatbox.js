e6ee887c3ae95a86751e75156d8c47902528bb69
ThanatipLnwza <Thanatip.ngan@bumail.net>
2021-03-06 09:21:54 +0700

    remove  header from chatbox

--- src/Chatbox.js
+++ src/Chatbox.js
@@ -19,14 +19,6 @@ function App() {
 
   return (
     <div className="App">
-      <div className="App-header">
-        Chatroom
-      </div>
-      <Link to="/friend-list">
-        <button>
-        FriendList
-        </button>
-      </Link>
       <div className="App-chatroom">
       {
         lines.map(x =>{

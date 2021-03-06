cf17f57c1de00de7ce1b311dd06152af8978ecd8
Chatmongkol <chatmongkol.phom@bumail.net>
2021-03-06 15:25:45 +0700

    1

--- src/FriendList.js
+++ src/FriendList.js
@@ -2,16 +2,26 @@ import {Link} from 'react-router-dom';
 
 
 function FriendList(){
-    return(
-        <div>
-            <Link to="/">
-                <button>
-                    Go to ChatRoom
-                </button>
-            </Link>
-        </div>
+    return (
+        <div className="FriendList">
+          <div className="FriendList-header">
         
-    )
-}
+          <div className="FriendList-Headers">
+          <Link to="/App">
+                    <botton>
+                        Go to chatroom
+                    </botton>
+                </Link>
+          </div>
+          </div>
+          <div className= "FriendList-Contact-info">
+          </div>
+          <div className="FriendList-chatroom">
+            {
+            }
+          </div>
+        </div>
+      );
+    };
 
 export default FriendList;

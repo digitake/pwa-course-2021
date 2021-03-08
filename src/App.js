9844c58b9baaf3fb39f0c51e0d060c6cdbf0b304
worawatkotc <worawat.kotc@bumail.net>
2021-03-08 14:02:37 +0700

    เพิ่ม Chatbox และแยกออกจาก App.js

--- src/App.js
+++ src/App.js
@@ -1,49 +1,27 @@
 import './App.css';
 import {useState} from 'react';
 import { Link } from "react-router-dom";
-
+import Chatbox from './Chatbox';
 
 function App() {
   const [text, setText] = useState("");
   const [lines, setLines] = useState([]);
-  
-  const onTextChange = (event) => {
-    setText(event.target.value);
-  };
 
-  const onSend = () =>{
+  const onBack = () =>{
     setLines([...lines, text]);
     setText("");
   };
 
-  const keyPress = (event) => {
-    if (event.which === 13) {
-      onSend();
-    }
-  }
-
   return (
-    <div className="App">
+    <div className="App col-6">
       <Link to="/friend-list">
-      <div className="App-textbox-back" onClick={onSend}>BACK</div>
+      <div className="App-textbox-back" onClick={onBack}>BACK</div>
       </Link>
       <div className="App-header">
         REACT CHAT
       </div>
-      <div className="App-chatroom">
-        {
-          lines.map(x => {
-            return <div className="App-chatroom-text">
-              {x}
-              </div>
-          })
-        }
-    </div>
-    <div className="App-textbox">
-      <input type="text" className="App-textbox-Input" 
-      value={text} onChange={onTextChange} onKeyPress={keyPress}/>
-
-      <div className="App-textbox-send" onClick={onSend}>Send</div>
+      <div className="App-content">
+        <Chatbox/>
       </div>
     </div>
   );
---
+++ src/Chatbox.js
@@ -0,0 +1,52 @@
+import './Chatbox.css';
+import {useState} from 'react';
+import { Link } from "react-router-dom";
+
+
+function Chatbox() {
+  const [text, setText] = useState("");
+  const [lines, setLines] = useState([]);
+  
+  const onTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const onSend = () =>{
+    setLines([...lines, text]);
+    setText("");
+  };
+
+  const keyPress = (event) => {
+    if (event.which === 13) {
+      onSend();
+    }
+  }
+
+  return (
+    <div className="Chatbox">
+      <Link to="/friend-list">
+      <div className="App-textbox-back" onClick={onSend}>BACK</div>
+      </Link>
+      <div className="App-header">
+        REACT CHAT
+      </div>
+      <div className="App-chatroom">
+        {
+          lines.map(x => {
+            return <div className="App-chatroom-text">
+              {x}
+              </div>
+          })
+        }
+    </div>
+    <div className="App-textbox">
+      <input type="text" className="App-textbox-Input" 
+      value={text} onChange={onTextChange} onKeyPress={keyPress}/>
+
+      <div className="App-textbox-send" onClick={onSend}>Send</div>
+      </div>
+    </div>
+  );
+}
+
+export default Chatbox;

fc211202ff220ee2bdd1a63a718209801dcadd3c
natchaphon_sirisangsawang <natchaphon.siri@bumail.net>
2021-03-10 21:54:10 +0700

    กำหมัดกับ Children เสร็จเเล้ว กำหมัดกับ Header flex-direction ต่อ

---
+++ src/LobbyHeader.js
@@ -0,0 +1,24 @@
+import './LobbyHeader.css';
+import {Link} from "react-router-dom";
+
+function LobbyHeader({children}) {
+    return (
+        <div className="App col-12">
+            <div className="App-header">
+                <div className="RoomName">
+                    <div className="profile-read-ju-on"/>
+                    <h1>Room Ju-on</h1>
+                    <h4>RID 123456</h4>
+                </div>
+                <Link to="/Profile_Tar">
+                    <div className="profile-read-tar"/>
+                </Link>
+                <div className="profile-read-pruek"/>
+                <div className="profile-read-phai"/>
+            </div>
+            {children}
+        </div>
+    );
+}
+
+export default LobbyHeader;
--- src/Router.js
+++ src/Router.js
@@ -1,34 +1,34 @@
-
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import App from "./App";
 import FriendList from "./FriendList";
 import EnterApp_3ofghost from "./EnterApp_3ofghost";
 import Eye_LoadingScene from "./Eye_LodeingScene";
 import Profiletar from "./Profile-Tar";
 import LobbySetting from "./LobbySetting";
+import ChatBox from "./Chatbox";
+import LobbyHeader from "./LobbyHeader";
 
-function Router(){
-    
-    return(
+function Router() {
+
+    return (
         <BrowserRouter>
             <Switch>
                 <Route path="/Profile_Tar">
-                  <Profiletar />
+                    <Profiletar/>
                 </Route>
                 <Route path="/Eye_Scene">
-                    <Eye_LoadingScene />
+                    <Eye_LoadingScene/>
                 </Route>
                 <Route path="/friend-list">
-                    <FriendList />
+                    <FriendList/>
                 </Route>
                 <Route path="/App">
-                    <App />
+                    <ChatBox/>
                 </Route>
                 <Route path="/LobbySetting">
-                    <LobbySetting />
+                    <LobbySetting/>
                 </Route>
                 <Route path="/">
-                    <EnterApp_3ofghost />
+                    <EnterApp_3ofghost/>
                 </Route>
             </Switch>
         </BrowserRouter>

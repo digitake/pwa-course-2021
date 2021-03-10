4f1539cfb4c04f69a290d16d41e470d8e22ff77b
tanawut19 <tanawut.jaem@bumail.net>
2021-03-10 21:31:04 +0700

    อัพเดทรูป

--- src/Store.js
+++ src/Store.js
@@ -1,6 +1,9 @@
 import{Link} from 'react-router-dom';
 import './App.css';
-import sticker1 from './sticker1.png';
+import sticker1 from './stickerA.png';
+import sticker2 from './stickerB.png';
+import sticker3 from './stickerC.png';
+import sticker4 from './stickerD.png';
 
 
 
@@ -10,12 +13,12 @@ function Store() {
 
        
         <div className="App-header-home">
-        Store
+        S
                        
     </div>
-<div className="App-test">
 
-</div>
+
+
 <div className="App-middle-home">
 
 
@@ -25,7 +28,7 @@ function Store() {
               <img src={sticker1}/>
               </Link> 
               <div className="App-text-left">
-              Sticker2 = 10$
+              Sticker1 = 10$
            
                </div>
            </div>  
@@ -35,10 +38,10 @@ function Store() {
 <div className="App-pic-left" >  
 
                <Link to="/home" >
-              <img src={sticker1}/>
+              <img src={sticker2}/>
               </Link> 
               <div className="App-text-left">
-              Sticker3 = 10$
+              Sticker2 = 10$
 
            </div>
 
@@ -49,10 +52,10 @@ function Store() {
 <div className="App-pic-left" >  
 
                <Link to="/home" >
-              <img src={sticker1}/>
+              <img src={sticker3}/>
               </Link> 
               <div className="App-text-left">
-               Sticker4 = 10$
+               Sticker3 = 10$
            </div>
 
 
@@ -61,10 +64,10 @@ function Store() {
 <div className="App-pic-left" >  
 
                <Link to="/home" >
-              <img src={sticker1}/>
+              <img src={sticker4}/>
               </Link> 
               <div className="App-text-left">
-              Sticker5 = 10$
+              Sticker4 = 10$
            </div>
 
 

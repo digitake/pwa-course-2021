acf139162220fec39096de4bf89ab849bc691fbe
NattaponRP <nattapon.rung@bumail.net>
2021-03-10 22:03:54 +0700

    Add friend profile

--- src/FriendProfile.js
+++ src/FriendProfile.js
@@ -1,11 +1,22 @@
 import App from './App';
+import './FriendProfile.css';
+import { Link } from 'react-router-dom'
+import backButton from './back_button.png'
+import profile from './profile.jpg'
 
 function FriendProfile(){
     return(
         
         <App>
-            <div>
-                My test
+            <div class="friendProfile">
+                <div class="friendProfile-box">
+                    <Link to="/friend-list">
+                        <img src={backButton}></img>
+                    </Link>
+                    <div class="friendProfile-detail">
+                        <img src={profile}></img>
+                    </div>
+                </div>
             </div>
         </App>
     )

df68a44a5fb056fe1a3226512ff728b8547f3685
Nattakan-phim <Nattakan.phim@gmail.net>
2021-03-11 22:48:59 +0700

    Update

--- src/LoginApp.js
+++ src/LoginApp.js
@@ -4,18 +4,20 @@ import './AppLogin.css'
 function LoginApp(){
     return(
         <div className = "LogoApp">
-             
-            <div className = "AppLogin">
+            
+            <div classname = "LogoE">
+            
+            </div>
 
+            <div className = "AppLogin">
                 <Link to = '/LoginPage'>
                     <img src="ButtonLogin.png"  alt = "" />
-                    
                 </Link>
             </div>
+
             <div className = "AppRegister">
                 <Link to = "/Register">
                 <img src = "ButtonRegister.png" alt = "" />
-                    
                 </Link>
             </div>
 

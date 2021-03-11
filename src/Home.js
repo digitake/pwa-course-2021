32b76fe896f917da6b99f7f97026755983a32d3a
natchapol-panm <natchapol.panm@bumail.net>
2021-03-12 03:52:14 +0700

    แก้ไขหน้า Home ครั้งที่1

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import './Home.css';
 import Chatbutton from './chat.png';
 import Friendbutton from './friend.png';
+import gif from './giphy.gif';
+import profile from './cool5.png';
 
 function Home() {
     return(
@@ -10,17 +12,35 @@ function Home() {
                 <div className ="link">
                 <Link to = "/chatbox">
                 <button>
-                    <img src ={Chatbutton} alt="" width="100" height="100"></img>
+                    <img src ={Chatbutton} alt="" width="75" height="75"></img>
                 </button>
                 </Link>
         </div>
         <div className ="link">
             <Link to ="/friend-list">
             <button>
-                <img src ={Friendbutton} alt="" width = "100" height = "100"></img>
+                <img src ={Friendbutton} alt="" width = "75" height = "75"></img>
             </button>
             </Link> 
         </div>
+        <div className ="link">
+            <Link to ="/profile1">
+                <button>
+                    <img src ={profile} alt="" width = "75" height = "75"></img>
+                </button>
+            </Link>
+        </div>
+
+        <div className = "Text">
+            Find
+            <div>
+            Friends
+        </div>
+        </div>
+        <div className = "gif">
+        <img src = {gif} alt= "" width = "300" height = "300"></img>
+        </div>
+
         </div>
         
 

c08bbc9a413300400ee32a45153a4ac187f7359d
Wisarut_Payonpaiboonsuk <wisarut.payo@bumail.net>
2021-03-11 18:28:35 +0700

    Layout Header เสจสิ้น

--- src/Router.js
+++ src/Router.js
@@ -1,36 +1,15 @@
 import {BrowserRouter, Switch , Route} from 'react-router-dom';
-<<<<<<< HEAD
-import Chatbox from './Chatbox';
-import ChatRoom from './ChatRoom';
-import ProFriend from './project/ProFriend';
-import FriendList from './FriendList';
-
-=======
 import Prologin from './project/Prologin';
 import Prosetting from './project/Prosetting';
 import ProFriend from './project/ProFriend';
 import Profile from './project/Profile';
 import ProChatroom from './project/ProChatroom';
->>>>>>> ab53e7d4fe1c2da023a6dd8f250365dcfa395295
 
 function Router() {
 
     return(
         <BrowserRouter>
             <Switch>
-<<<<<<< HEAD
-                <Route path="/chatroom">
-                    <ChatRoom />
-                </Route>
-                <Route path="/fri">
-                    <FriendList />
-                </Route>
-                <Route path="/ProFriend">
-                    <ProFriend />
-                </Route>
-                <Route path="/">
-                    <Chatbox />
-=======
                 <Route path = "/Prosetting">
                     <Prosetting />
                 </Route>
@@ -45,7 +24,6 @@ function Router() {
                 </Route>
                 <Route path = "">
                     <Prologin />
->>>>>>> ab53e7d4fe1c2da023a6dd8f250365dcfa395295
                 </Route>
             </Switch>
         </BrowserRouter>
--- src/project/ProFriend.js
+++ src/project/ProFriend.js
@@ -1,87 +1,53 @@
 import './Protheme.css';
 import { Link } from 'react-router-dom';
+import HeaderLayout from './HeaderLayout';
 
 
 
 function ProFriend() {
   return (
   <div className="App">
-    <div className="App-header">
-      <div className="circlesbox4">  
-<<<<<<< HEAD
-        <Link to='/ProSetting'>
-          <img src="3Line.png" alt="3Line.png" width="50px" height="50px"/>         
-=======
-        <Link to='/Prosetting'>
-          <img src="/Project/ProFriend/3Line.png" alt="/Project/ProFriend/3Line.png" width="50px" height="50px"/>         
->>>>>>> ab53e7d4fe1c2da023a6dd8f250365dcfa395295
-        </Link>
-      </div>
-      <div className="text">
-      Hoshimati Suisei
-      </div>
-      <div className="Profile">    
-<<<<<<< HEAD
-        <Link to='/ProProfile'>
-            <img src="Sui.jpg" alt="Sui.jpg" width="75px" height="75px" />          
-=======
-        <Link to='/Profile'>
-          <button>
-            <img src="/Project/ProFriend/Sui.jpg" alt="/Project/ProFriend/Sui.jpg" width="75px" height="75px" />
-          </button>
->>>>>>> ab53e7d4fe1c2da023a6dd8f250365dcfa395295
-        </Link>
-       </div>
-    </div>
+    <HeaderLayout>
+            <div className="Textlayout">
+                Hoshimati Suisei
+            </div>
+            <div className="Profilepicture">    
+                <Link to='/Profile'>
+                    <img src="/Project/ProFriend/Sui.jpg" alt="/Project/ProFriend/Sui.jpg" width="75px" height="75px" />
+                </Link>
+            </div>
+    </HeaderLayout>
     <div className="Textline">
       <div className="Textinline">FriendList</div>  
     </div>
     <div className="App-chatroom">    
-      <div className="circlesbox1">    
+      <div className="circlesboxRut1">    
         <Link to='/ProChatroom'>
-<<<<<<< HEAD
-            <img src="sad.png" alt="sad.png" width="100px" height="100px" />
-=======
-          <button>
             <img src="/Project/ProFriend/sad.png" alt="/Project/ProFriend/sad.png" width="100px" height="100px" />
-          </button>
->>>>>>> ab53e7d4fe1c2da023a6dd8f250365dcfa395295
         </Link> 
       </div>
       <div className="text1">
         Pepe1
        </div>
-      <div className="circlesbox2">    
+      <div className="circlesboxRut2">    
         <Link to='/ProChatroom'>
-<<<<<<< HEAD
-            <img src="sad.png" alt="sad.png" width="100px" height="100px"/>       
-=======
-          <button>
             <img src="/Project/ProFriend/sad.png" alt="/Project/ProFriend/sad.png" width="100px" height="100px"/>
-          </button>
->>>>>>> ab53e7d4fe1c2da023a6dd8f250365dcfa395295
         </Link> 
       </div>
       <div className="text2">
         Pepe2
        </div>
-      <div className="circlesbox3">    
+      <div className="circlesboxRut3">    
       <Link to='/ProChatroom'>
-<<<<<<< HEAD
-            <img src="nono.jpg" alt="nono.jpg" width="100px" height="100px" />
-      </Link> 
-=======
-          <button>
             <img src="/Project/ProFriend/nono.jpg" alt="/Project/ProFriend/nono.jpg" width="100px" height="100px" />
-          </button>
       </Link>
->>>>>>> ab53e7d4fe1c2da023a6dd8f250365dcfa395295
         </div>
         <div className="text3">
           Noel
           </div>
     </div>
 </div>
+
   );
 }
 export default ProFriend;

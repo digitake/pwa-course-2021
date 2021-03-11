d712d69b5aa45e8d2a833666ec608e6c5d1990fd
TeerathepS <teerathep.saia@bumail.net>
2021-03-11 16:55:23 +0700

    เปลี่ยนชื่อไฟล์ lodeing.js เป็น Loading.js และแยก script ตัวปุ่มออกมา

---
+++ src/ButtonLobbySettings.js
@@ -0,0 +1,52 @@
+﻿import {Link} from "react-router-dom";
+import './LobbySetting.css';
+
+
+function ButtonLobbySetting({children}) {
+    
+    return (
+        <div className="LoadingSetting">
+            <div className="Private">
+                <h1>Private Room</h1>
+            </div>
+            <div className="AllowHint">
+                <h1>AllowHint</h1>
+            </div>
+            <div className="AllowJoin">
+                <h1>Allow to Join between game</h1>
+            </div>
+            <div className="PlayerName">
+                <h1>Show player's name</h1>
+            </div>
+
+            <label className="switch">
+                <input type="checkbox"/>
+                <span className="slider round">
+                </span>
+                {children}
+            </label>
+
+            <label className="switch">
+                <input type="checkbox"/>
+                <span className="slider round">
+                </span>
+                {children}
+            </label>
+
+            <label className="switch">
+                <input type="checkbox"/>
+                <span className="slider round">
+                </span>
+                {children}
+            </label>
+
+            <label className="switch">
+                <input type="checkbox"/>
+                <span className="slider round">
+                </span>
+            </label>
+            {children}
+        </div>
+    );
+}
+export default ButtonLobbySetting;

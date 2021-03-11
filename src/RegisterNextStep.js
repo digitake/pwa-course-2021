18763bb1aadbfddcdba98f5d507da1a1201e9b3a
Nattakan-phim <Nattakan.phim@gmail.net>
2021-03-12 01:17:48 +0700

    update Register

--- src/RegisterNextStep.js
+++ src/RegisterNextStep.js
@@ -1,19 +1,31 @@
 import {Link} from 'react-router-dom'
-import './App.css';
+import './AppRegister.css';
 
 function RegisterNextStep() {
     return(
         <div className = "NextStep">
-           <div className = "Undo">
-             <Link to = "/Register">
-                <img src = "Union.png"  alt = ""/>
-             </Link>
-             </div>
-             <div className = "SingUP">
-            <Link to = "/HomePage">
-                <img src = "ButtonSingUP.png" alt = "" />
-            </Link>
-             </div>
+
+            <div className = "Undo">
+                <Link to = "/Register">
+                    <img src = "Union.png"  alt = ""/>
+                </Link>
+
+            </div>
+            <div className = "Register-Logo"/>
+
+            <div className = "Register-singIn"/>
+
+
+            <div className = "Register-recapcha"/>
+        
+            <div className = "SingUP">
+
+                <Link to = "/HomePage">
+                    <img src = "ButtonSingUP.png" alt = "" />
+                </Link>
+
+            </div>
+
         </div>
     )
 
--- src/Rigister.js
+++ src/Rigister.js
@@ -1,20 +1,27 @@
-import './AppLogin.css'
+import './AppRegister.css'
 import {Link} from 'react-router-dom'
 
 
 function Register(){
     return(
     <div className = "Register">
+
         <div className = "Undo">
              <Link to = "/">
                 <img src = "Union.png"  alt = ""/>
              </Link>
         </div>
 
+        <div className = "Register-Logo"/>
+
+        <div className = "Register-singIn"/>
+
         <div className = "RegisterNextStep">
-        <Link to = "/RegisterNextStep">
-            <img src = "ButtonNext.png" alt = "" /> 
-        </Link>
+
+            <Link to = "/RegisterNextStep">
+                <img src = "ButtonNext.png" alt = "" /> 
+            </Link>
+
         </div>
 
     </div>

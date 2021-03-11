581fbbb8643ddbbfc06babe61e96c223a1716edc
newpuwinwin <puwin.meep@bumail.net>
2021-03-11 21:12:46 +0700

    Update Grouplist and PapFriendlist กันหน่อยวัยรุ่น

--- src/FriendList.js
+++ src/FriendList.js
@@ -1,6 +1,7 @@
 import{Link} from 'react-router-dom';
 import './App.css';
 import App from './App';
+
 function FriendList(){
     return(
         <App>
--- src/Profile.js
+++ src/Profile.js
@@ -1,14 +1,14 @@
 import App from './App';
-
+import{Link} from 'react-router-dom';
+import './App.css';
+import Groupchat1 from './Groupchat1.png';
 
 function Profile()
 {return(
 <App>
-
-    <div>
-        Profile
-        
-    </div>
+<div>
+    
+</div>
 </App>
 )
 };

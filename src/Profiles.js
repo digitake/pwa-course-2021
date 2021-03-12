3538395ba3d3fb2ca21f1efdd6fc66bb05c77743
Barrower990 <king2ok555@gmail.com>
2021-03-12 17:42:24 +0700

    Do_Profile_Respeonsive

--- src/Profiles.js
+++ src/Profiles.js
@@ -6,25 +6,7 @@ function profiles() {
     return (
     <div className="Page">
             <div className="contain">
-                Profiles
-               <ul className="Function">
-                   <li className="Function-link">
-                   <a href="#"><FiFacebook/>Facebook</a>
-                   </li>
-                   <li className="Function-link">
-                   <a href="#"><FiInstagram/>Instagram</a>
-                   </li>
-                   <li className="Function-link">
-                   <a href="#"><FiTwitter/>Twitter</a>
-                   </li>
-                   </ul> 
-        <div className="App-contents">
-        </div>
-        <Link to="/PrivateChat">
-        <button>
-        PrivateChatRoom
-        </button>
-        </Link>
+            
             </div>
         </div>
     );

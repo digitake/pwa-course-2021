1049834209a23b7bd5e7bd412fa081c637b615f9
Barrower990 <king2ok555@gmail.com>
2021-03-12 19:50:55 +0700

    Profile_update

--- src/Profiles.js
+++ src/Profiles.js
@@ -1,14 +1,54 @@
 import './Profiles.css';
-import { FiFacebook,FiInstagram,FiTwitter } from "react-icons/fi";
+import {GrFacebook , GrGithub ,GrTwitter} from "react-icons/gr";
+import {CgProfile, CgInstagram} from "react-icons/cg";
 import { Link } from 'react-router-dom';
 
 function profiles() {
     return (
-    <div className="Page">
-            <div className="contain">
+    <div className="Header">
+            <div className = "Profile-Titles">
+                Profile
+            </div>
+
+        <div className = "Info">
+
+                <div className = "Profile" >
+                    <CgProfile/>
+                    <div classname = "InfoProfile">
+                        Profile
+                    </div>
+                </div>
+
+                <div className = "Facebook">
+                    <GrFacebook />
+                    <div className = "InfoFacebook">
+                        Thanaporn Tungyen
+                    </div>
+                </div>
             
+                <div className = "Instagram">
+                    <CgInstagram/>
+                    <div className = "InfoInstagram">
+                        I LOVE FACEBOOK BETTER!
+                    </div>
+                </div>
+
+                <div className = "Github">
+                    <GrGithub/>
+                    <div className = "InfoGithub">
+                    https://github.com/Barrower990/pwa-course-2021
+                    </div>
+                </div>
+
+                <div className = "Twitter">
+                    <GrTwitter/>
+                        <div classname = "InfoTwitter">
+                            Twitter is take over by flametwit
+                        </div>
+                </div>
             </div>
         </div>
+        
     );
 }
 

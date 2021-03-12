4bccb98ae43384e1d26e0998deba17adc476472d
adisorn080843 <adisorn.numk@bumail.net>
2021-03-12 21:08:45 +0700

    mage จากมิกกี้

--- src/project/Prologin.js
+++ src/project/Prologin.js
@@ -12,30 +12,17 @@ function Prologin({children}) {
       <div className ="background-login">
          
              <div to="/" className="Prologin-logo">
-<<<<<<< HEAD
-                 <img src= "/images/boxchat.jpg" alt = "/images/boxchat.jpg" 
-               width="370px" height="290px"/> 
-             </div>
-
-             <Link to="/" className="signin-logo">
-                 <img src= "/images/sign.jpg" alt = "/images/sign.jpg"
-=======
                  <img src= "/Project/Prologin/boxchat.jpg" alt = "/Project/Prologin/boxchat.jpg" 
                width="370px" height="290px"/> 
              </div>
 
              <Link to="/Profriend" className="signin-logo">
                  <img src= "/Project/Prologin/sign.jpg" alt = "/Project/Prologin/sign.jpg"
->>>>>>> 433c972f817d0429cf5ab09923f22c688ed4316d
                width="320px" height="85px"/> 
              </Link>
 
              <Link to="/" className="register-logo">
-<<<<<<< HEAD
-                 <img src= "/images/register.jpg" alt = "/images/register.jpg" 
-=======
                  <img src= "/Project/Prologin/register.jpg" alt = "/Project/Prologin/register.jpg" 
->>>>>>> 433c972f817d0429cf5ab09923f22c688ed4316d
                width="240px" height="70px"/> 
              </Link>
 

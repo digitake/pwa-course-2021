9f20995fc504c0a350c6ec7fd8d25583a53d3919
Pknw260643 <kanokwan.janta@bumail.net>
2021-03-12 18:27:29 +0700

    สร้าง creat tee และรอแก้โค้ต

---
+++ src/Creat Tee.js
@@ -0,0 +1,15 @@
+
+<div>
+  <form action="/action_page.php">
+    <label for="fname">group</label>
+    <input type="text" id="fname" name="creat team a group" placeholder="creat team a group.">
+
+    <label for="lname">activity</label>
+    <input type="text" id="lname" name="activity name" placeholder="activity name.">
+
+    <label for="lname">Date</label>
+    <input type="text" id="lname" name="date" placeholder="date.">
+
+    <input type="submit" value="Submit">
+  </form>
+</div>

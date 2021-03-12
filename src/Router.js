2647a5e20b756549ebf1b5ca7b2018b678f4234a
adisorn080843 <adisorn.numk@bumail.net>
2021-03-12 21:23:18 +0700

    homework 12/3/64

--- src/Router.js
+++ src/Router.js
@@ -1,22 +1,15 @@
 import {BrowserRouter, Switch , Route} from 'react-router-dom';
 import Prologin from './project/Prologin';
-<<<<<<< HEAD
-
-=======
 import Prosetting from './project/Prosetting';
 import ProFriend from './project/ProFriend';
 import Profile from './project/Profile';
 import ProChatroom from './project/ProChatroom';
->>>>>>> 433c972f817d0429cf5ab09923f22c688ed4316d
 
 function Router() {
 
     return(
         <BrowserRouter>
             <Switch>
-<<<<<<< HEAD
-                <Route path = "/Prologin">
-=======
                 <Route path = "/Prosetting">
                     <Prosetting />
                 </Route>
@@ -30,7 +23,6 @@ function Router() {
                     <ProChatroom />
                 </Route>
                 <Route path = "">
->>>>>>> 433c972f817d0429cf5ab09923f22c688ed4316d
                     <Prologin />
                 </Route>
             </Switch>

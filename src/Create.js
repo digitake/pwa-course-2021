2dbf3df4398ef5041cf5e0540033e0ae7084cdce
Pknw260643 <kanokwan.janta@bumail.net>
2021-03-12 21:53:39 +0700

    แก้ไข creat

---
+++ src/Create.js
@@ -0,0 +1,19 @@
+import './Create.css';
+
+function CreateTee() {
+ return(
+     <div className = "Create">
+         <header className = "create header">
+             <h4> Status </h4>
+         </header>
+         <div className = "creat img" > 
+         <img src = {st.jpg} alt ="" width = "300px" height ="300px"></img>
+         </div>
+    </div>
+
+        
+ )
+};
+        
+  
+

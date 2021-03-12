358f1f6dd88da05f60af74677867db94484b6a59
poomphan1620701944 <poomphan.meen@bumail.net>
2021-03-12 17:38:45 +0700

    This is a new commit for what I originally planned to be amended

--- src/App.js
+++ src/App.js
@@ -1,45 +1,32 @@
 import './App.css';
-import {useState} from 'react';
+import { Link } from 'react-router-dom';
 
-function App() {
-  const [text, setText] = useState("");
-  const [lines, setLines] = useState([]);
-  
-  const onTextChange = (event) => {
-    setText(event.target.value);
-  };
-
-  const onSend = () =>{
-    setLines([...lines, text]);
-    setText("");
-  };
-
-  const keyPress = (event) => {
-    if (event.which === 13){
-      onSend();
-    }
-  };
+function App({children}) {
 
   return (
     <div className="App">
       <div className="App-header">
-        DOGEBOOK
-      </div>
-      <div className="App-chatroom">
-        {
-          lines.map(x => {
-            return <div className="App-chatroom-text">
-                    {x}
-                   </div>
-          })
-        }
-      </div>
-      <div className="App-textbox">
-        <input type="text" className="App-textbox-input" 
-        value={text} onChange={onTextChange} onKeyPress={keyPress}/>
-        
-        <div className="App-textbox-send" onClick={onSend}>Send!</div>
-      </div>
+              <Link to="friend-list">
+                    <button>
+                         Friendlist
+                    </button>
+              </Link>
+              
+              <Link to="Profile">
+                    <button>
+                         Profile
+                    </button>
+              </Link>
+
+              <Link to="/">
+                    <button>
+                         Chatbox
+                    </button>
+              </Link>
+          </div>  
+          <div className="App-content">
+            {children}
+          </div>
     </div>
   );
 }

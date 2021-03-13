eaa56d87038574f527d9deda933bd24e975a1eeb
mantawit_ramsomphop <mantawit.rams@bumail.net>
2021-03-13 15:38:37 +0700

    แก้ไขFriendlist

--- src/FriendList.js
+++ src/FriendList.js
@@ -1,21 +1,15 @@
-/*import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import App from './App';
 
 function FriendList() {
     return(
         <App>
             <h1>Add friend</h1>
-            <p class="solid">Search by iD / Phone</p>
-            <input type="text" placeholder="Search.." name="search2"></input>
-            <select id="country" name="country">
-                <option value="au">ID</option>
-                <option value="ca">Phone No</option>
-            </select>
-            <img width="200" height="200"></img>
+            
         </App>
     )
     
 }
 
 
-export default FriendList;*/
+export default FriendList;

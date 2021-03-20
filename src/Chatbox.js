bba09bfbc82550cdf7f9ffcc4617e00a3dce2b24
natchaphon_sirisangsawang <natchaphon.siri@bumail.net>
2021-03-20 10:05:25 +0700

    เพิ่ม Sender Timestamp message

--- src/Chatbox.js
+++ src/Chatbox.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import './ChatBox.css';
 import LobbyHeader from "./LobbyHeader";
 
@@ -6,16 +6,25 @@ function ChatBox({children}) {
 
     const [text, setText] = useState("");
 
-    const [lines, setLines] = useState(["Tar : Hello guys!"]);
+    const [lines, setLines] = useState([]);
+    
+    // () => {} same as function() {}  --lambda notation
+   // useEffect(() => {alert("total text chat =" + lines.length)}, [lines]);
 
     const onTextChange = (event) => {
         setText(event.target.value);
     };
 
     const onSend = () => {
-        setLines(line => [...lines, "Me : " + text]);
+        setLines(line => [...lines, {
+            sender: "Me : ",
+            message: text,
+            timestamp: (new Date())
+        }]);
         setText("")
     }
+    
+    const keyPress = (event) =>{if (event.which === 13){onSend();}};
 
     return (
         <LobbyHeader>
@@ -24,7 +33,9 @@ function ChatBox({children}) {
                     {
                         lines.map(x => {
                             return <div className="App-chatroom-text">
-                                {x}
+                                {x.sender}
+                                {x.message}
+                                <div>{x.timestamp.toLocaleString()}</div>
                             </div>  
                         })
                     }
@@ -38,7 +49,8 @@ function ChatBox({children}) {
                     </div>
                     <input type="text" placeholder="Type Ready to Start!" className="App-textbox-input col-9"
                            value={text}
-                           onChange={onTextChange}/>
+                           onChange={onTextChange}
+                           onKeyPress={keyPress}/>
                     <div className="App-textbox-send col-2" onClick={onSend}>Send!</div>
                 </div>
                 

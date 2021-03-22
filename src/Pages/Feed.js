3020adf9a913a7d16384db880c4ac88702a77d98
jay <jayjay958@gmail.com>
2021-03-22 20:49:04 +0700

    Update Feed: Edit, Delete option

--- src/Pages/Feed.js
+++ src/Pages/Feed.js
@@ -45,10 +45,25 @@ function Comment(props) {
     );
 }
 
-function Delete(props) {
+function Option(props) {
+    const toggle = () => ToggleOption(props.popup);
+    const onEdit = () => { alert('Edit post id: ' + props.popup) }
+    const onDelete = () => { alert('Delete post id: ' + props.popup) }
     return (
-        <a className='delete'> •••</a>
+        <div className='option'>
+            <div className='optionText' onClick={toggle}>•••</div>
+            <div className='myPopup' id={props.popup} onClick={toggle}>
+                <div className='optionBox edit' onClick={onEdit}>Edit</div>
+                <div className='optionBox delete' onClick={onDelete}>Delete</div>
+            </div>
+
+        </div>
     );
+}
+
+function ToggleOption(i) {
+    var popup = document.getElementById(i);
+    popup.classList.toggle('show');
 }
 
 class Feed extends Component {
@@ -63,7 +78,7 @@ class Feed extends Component {
                             name='Elon Musk'
                             id='real.elon'
                             date='2 days ago' />
-                        <Delete />
+                        <Option popup='1' />
                         <Post text='
                       " People work better when they know what the goal is and why. It is important that people look forward to coming to work in the morning and enjoy working. "' />
                         <Comment text='5,023 Comments' />
@@ -75,7 +90,7 @@ class Feed extends Component {
                             name='Custom Name'
                             id='UserID'
                             date='2 weeks ago' />
-                        <Delete />
+                        <Option popup='2' />
                         <Post text='
                       Just take my hand, lead, dance with me...and I will simply follow the blueness of the water, the white waves rolling free...where the earth beneath my feet and stars make my heart whole again...in long and priceless moments of shared solitude...
                         ' />

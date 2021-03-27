6a191b6168e94a75269c83db95b2ca7eae42f180
natchaphon_sirisangsawang <natchaphon.siri@bumail.net>
2021-03-27 10:07:50 +0700

    import firebase to chatbox

--- src/Chatbox.js
+++ src/Chatbox.js
@@ -1,30 +1,46 @@
-import React, {useState,useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import './ChatBox.css';
-import LobbyHeader from "./LobbyHeader";
+import LobbyHeader from "./LobbyHeader"
+import firebase from "./firebaseConfig";
+
+const chatroomRef = firebase.database().ref('fantasma-room-1');
 
 function ChatBox({children}) {
 
     const [text, setText] = useState("");
 
     const [lines, setLines] = useState([]);
-    
+
     // () => {} same as function() {}  --lambda notation
-   // useEffect(() => {alert("total text chat =" + lines.length)}, [lines]);
+    useEffect(() => {
+        chatroomRef.on('child_added', snapshot => {
+            let x = snapshot.val();
+            setLines(l => [...l, {
+                sender: x.sender,
+                message: x.message,
+                timestamp: (new Date())
+            }])
+        });
+    }, []);
 
     const onTextChange = (event) => {
         setText(event.target.value);
     };
 
     const onSend = () => {
-        setLines(line => [...lines, {
+        // push to firebase sever
+        chatroomRef.push({
             sender: "Me : ",
             message: text,
-            timestamp: (new Date())
-        }]);
+        });
         setText("")
     }
-    
-    const keyPress = (event) =>{if (event.which === 13){onSend();}};
+
+    const keyPress = (event) => {
+        if (event.which === 13) {
+            onSend();
+        }
+    };
 
     return (
         <LobbyHeader>
@@ -35,28 +51,29 @@ function ChatBox({children}) {
                             return <div className="App-chatroom-text">
                                 {x.sender}
                                 {x.message}
-                                <div className="App-Time-Position">{ " --- " + x.timestamp.toLocaleString() + " --- "}</div>
-                                
-                            </div>  
+                                <div
+                                    className="App-Time-Position">{" --- " + x.timestamp.toLocaleString() + " --- "}</div>
+
+                            </div>
                         })
                     }
                 </div>
                 {children}
-                
+
                 {/*ChatBar Input*/}
                 <div className="App-textbox">
                     <div className="App-Botton col-1">
-                   </div>
+                    </div>
                     <input type="text" placeholder="Type Ready to Start!" className="App-textbox-input col-9"
                            value={text}
                            onChange={onTextChange}
                            onKeyPress={keyPress}/>
                     <div className="App-textbox-send col-2" onClick={onSend}>Send!</div>
                 </div>
-                
+
             </div>
-            
-           
+
+
         </LobbyHeader>
     );
 }

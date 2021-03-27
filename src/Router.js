613ff8e4df657a8a2e1a4637193b0af7a69e343c
digitake <digitake@gmail.com>
2021-03-27 08:16:19 +0700

    Flexbox

--- src/Router.js
+++ src/Router.js
@@ -3,12 +3,16 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import FriendList from './FriendList';
 import Chatbox from './Chatbox';
 import Profile from './Profile';
+import FlexShow from './FlexShow';
 
 function Router() {
 
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/flex">
+          <FlexShow />
+        </Route>
         <Route path="/profile">
           <Profile />
         </Route>

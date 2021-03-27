d4ac2d707012bfe430b19d95ecb62b529642635e
ThanatipLnwza <Thanatip.ngan@bumail.net>
2021-03-27 10:43:33 +0700

    add server timestamp

--- src/Chatbox.js
+++ src/Chatbox.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from 'react';
 import './Chatbox.css';
 import Headerchat from './Headerchat';
 
+import firebase from './firebaseConfig'; //ดึง config
+
+const chatroomRef = firebase.database().ref('chatroom-1');
+
 function Chatbox() {
 
   const [text, setText] = useState("")
@@ -10,10 +14,15 @@ function Chatbox() {
 // () => {} same as function() {}
   
 useEffect(() => {
-    //alert("Total text chat = " + lines.length) //Hook
-    //sendDatatoserver()
-
-  }, [lines]);
+    chatroomRef.on('child_added', snapshot => {
+      let x = snapshot.val();
+      setLines(l => [...l, {
+        sender: x.sender, 
+        message : x.message,
+        timestamp: (new Date(x.timestamp))
+      }])
+    })
+  }, []);
   //watchdog
 
   const onTextChange = (event) => {
@@ -21,13 +30,14 @@ useEffect(() => {
   };
 
   const onSend = () => {
-    setLines(lines => [...lines, {
-      sender:"Me", 
-      message :text,
-      timestamp: (new Date()),
-    }
-    ]);
-    setText("");
+   // Push message to firebase server
+   chatroomRef.push({
+     sender : "Me",
+     messsage : text,
+     timestamp: firebase.database.ServerValue.TIMESTAMP
+   })
+    
+   setText("");
   };
 
   return (
@@ -53,7 +63,7 @@ useEffect(() => {
       <div className="App-textbox">
         <button class="btn"><i>More Function</i></button>
         <input type="text" className="App-textbox-input" value={text} onChange={onTextChange} placeholder="Type a message" />
-        <div className="App-textbox-send" onClick={onSend}>Send</div>
+        <div className="App-textbox-send" onClick={onSend}>Send </div>
       </div>
     </div>
     </Headerchat>

3a55b7420af449e107d6be7dbc7630dd86f3fc90
lapassuth <lapas.suth@bumail.net>
2021-03-27 16:40:19 +0700

    firebase but fail

--- src/Chatbox.js
+++ src/Chatbox.js
@@ -1,18 +1,43 @@
 
-import './Chatbox.css'
-import {useState} from 'react'
+import './Chatbox.css';
+import {useEffect, useState} from 'react';
 import App from './App';
+
+import firebase from './firebaseConfig';
+
+const chatroomRef = firebase.database().ref('chatroom-1');
+
 function Chatbox() {
 
   const [text,setText] = useState("");
-  const [lines,setLines] = useState(["","","",""]);
+  const [lines,setLines] = useState([]);
+  
+  useEffect(() => {
+    chatroomRef.on("cild_added", snapshot =>{
+      let x = snapshot.val();
+
+      setLines(line => [...line, {
+        sender: x.sender,
+        massage: x.massage,
+        timestamp: new Date(x.timestamp)
+      }])
+    });
+  }, []);
+
   const onTextChange = (event) => {
     setText(event.target.value);
   };
+
   const onSend =() =>{
-    setLines([...lines, text])
+    chatroomRef.push({
+      sender: "Pus",
+      massage: text,
+      timestamp: firebase.database.ServerValue.TIMESTAMP
+    })
+
     setText("");
   };
+
   const keyPress = (event) =>  {
     if(event.which===13){
       onSend();
@@ -39,6 +64,7 @@ function Chatbox() {
         </div>
         <div className="App-chatroom-text">
           {
+
           lines.map(x => {
           return <div className="App-chatroom-text">
                   {x}
---
+++ src/firebaseConfig.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBwVGhfkKtVjgcpOlwYCUSm0yyiOr2VCU4",
+    authDomain: "test1-ff6dc.firebaseapp.com",
+    databaseURL: "https://test1-ff6dc-default-rtdb.firebaseio.com/",
+    projectId: "test1-ff6dc",
+    storageBucket: "test1-ff6dc.appspot.com",
+    messagingSenderId: "1091443552151",
+    appId: "1:1091443552151:web:b90e4c8db912ff9b0a1ed2",
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase;
+

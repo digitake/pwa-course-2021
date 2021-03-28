25dc2b2ef7185c9fdb6f21df750d48f40760da35
jay <jayjay958@gmail.com>
2021-03-28 21:46:44 +0700

    Update Chatbox: Able to change displayname in chat

--- src/Chatbox.js
+++ src/Chatbox.js
@@ -3,7 +3,8 @@ import App from './App';
 import firebase from './firebaseConfig';
 import './Chatbox.css';
 
-const chatroomRef = firebase.database().ref('chatroom-1');
+const chatroomRef = firebase.database().ref('chatroom-2');
+let senderName = '';
 
 function Chatbox() {
     const [text, setText] = useState('');
@@ -24,8 +25,12 @@ function Chatbox() {
 
     const onSend = () => {
         if (text.length < 1) return;
+        if (senderName.length < 1) {
+            alert("Please set your displayname");
+            return;
+        }
         chatroomRef.push({
-            sender: "Who Is This?",
+            sender: senderName,
             message: text,
             timestamp: firebase.database.ServerValue.TIMESTAMP,
         });
@@ -41,8 +46,33 @@ function Chatbox() {
             onSend();
         }
     }
+
+    const onNameChange = (event) => {
+        event.preventDefault();
+        const input = document.getElementById('fname');
+        input.classList.toggle('name-show');
+        if (input.className === 'name-input') {
+            senderName = input.value;
+            document.getElementById('fsubmit').value = senderName;
+        } else {
+            document.getElementById('fsubmit').value = 'Done';
+        }
+    }
+
+    const ChangeName = () => {
+        return (
+            <div>
+                <form id='displayname' className='displayname-settings' onSubmit={onNameChange}>
+                    <input className='name-input' id='fname' placeholder='Displayname...' type='text' />
+                    <input className='name-submit' type="submit" id='fsubmit' value='Change Name' />
+                </form>
+            </div>
+        );
+    }
+
     return (
         <App>
+            <ChangeName />
             <div className="App-chatroom">
                 {
                     lines.map(x => {
@@ -64,7 +94,7 @@ function Chatbox() {
             </div>
             <div>
             </div>
-        </App>
+        </App >
     );
 }
 

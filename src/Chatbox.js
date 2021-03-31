83048dbea0df7f97b8a8e417793ce4d08488179d
chatchaiphat924 <chatchiphat@outlook.com>
2021-03-31 10:16:47 +0700

    pull_week_10

--- src/Chatbox.js
+++ src/Chatbox.js
@@ -1,6 +1,9 @@
-import React, {useState,useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import './ChatBox.css';
-import LobbyHeader from "./LobbyHeader";
+import LobbyHeader from "./LobbyHeader"
+import firebase from "./firebaseConfig";
+
+const chatroomRef = firebase.database().ref('fantasma-room-1');
 
 import firebase from './firebaseConfig';
 
@@ -11,18 +14,17 @@ function ChatBox({children}) {
     const [text, setText] = useState("");
 
     const [lines, setLines] = useState([]);
-    
-    // () => {} same as function() {}  --lambda notation
-   useEffect(() => {
-       chatroomRef.on('child_added',snapshot =>{
-           let x = snapshot.val();
-           setLines(l => [...l, {
-            sender: x.sender,
-            message: x.message,
-            timestamp: (new Date(x.timestamp))
-       }])
-    });
 
+    // () => {} same as function() {}  --lambda notation
+    useEffect(() => {
+        chatroomRef.on('child_added', snapshot => {
+            let x = snapshot.val();
+            setLines(l => [...l, {
+                sender: x.sender,
+                message: x.message,
+                timestamp: (new Date(x.timestamp))
+            }])
+        });
     }, []);
 
     const onTextChange = (event) => {
@@ -30,15 +32,20 @@ function ChatBox({children}) {
     };
 
     const onSend = () => {
+        // push to firebase sever
         chatroomRef.push({
-            sender : "Spector" ,
-            message : text,
-            timestamp:firebase.database.ServerValue.TIMESTAMP
+            sender: "Me",
+            message: text,
+            timestamp: firebase.database.ServerValue.TIMESTAMP
         });
         setText("")
+    }
+
+    const keyPress = (event) => {
+        if (event.which === 13) {
+            onSend();
+        }
     };
-    
-    const keyPress = (event) =>{if (event.which === 13){onSend();}};
 
     return (
         <LobbyHeader>
@@ -47,19 +54,20 @@ function ChatBox({children}) {
                     {
                         lines.map(x => {
                             return <div className="App-chatroom-text">
-                                {x.sender+':'}
-                                {x.message}
-                                <div>{x.timestamp.toLocaleString()}</div>
-                            </div>  
+                                <div className="App-textPosition">
+                                    {x.sender + " : "}
+                                    {x.message}
+                                </div>
+                                <div className="App-Time-Position"> {x.timestamp.toLocaleTimeString()} </div>
+                            </div>
                         })
                     }
                 </div>
                 {children}
-                
+
                 {/*ChatBar Input*/}
                 <div className="App-textbox">
                     <div className="App-Botton col-1">
-                        <img src="botton.png"/>
                     </div>
                     <input type="text" placeholder="Type Ready to Start!" className="App-textbox-input col-9"
                            value={text}
@@ -67,10 +75,10 @@ function ChatBox({children}) {
                            onKeyPress={keyPress}/>
                     <div className="App-textbox-send col-2" onClick={onSend}>Send!</div>
                 </div>
-                
+
             </div>
-            
-           
+
+
         </LobbyHeader>
     );
 }

079b163c4d8845e286355954bb778ec2b57afc4d
mantawit_ramsomphop <mantawit.rams@bumail.net>
2021-04-03 10:27:35 +0700

    This is a new commit for what I originally planned to be amended

--- src/Chatbox.js
+++ src/Chatbox.js
@@ -1,17 +1,40 @@
 import './Chatbox.css';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import App from './App';
+import firebase from './firebaseConfig' // ส่งคอนฟิกมาใช้งาน
+
+const chatroomRef = firebase.database().ref('chatroom-1');
+
 
 function Chatbox() {
   const [text, setText] = useState("");
   const [lines, setLines] = useState([]);
+  // ["-้กดกด","กกกกก","กกกหกดหกดหกด"]
+  // [{sender: "Mon", message: "skldfsjdlkfsd", timestamp: 12387687437424}, {sender: "Mon", message: "slkdjfsdlkfline2", timestamp: 123434234}]
+
+  useEffect(() => {
+    chatroomRef.on('child_added', snapshot => {
+      let x = snapshot.val();
+      setLines(l => [...l, {
+        sender: x.sender,
+        message: x.message,
+        timestamp: (new Date())
+      }])
+    });
+
+  }, []);
 
   const onTextChange = (event) => {
     setText(event.target.value);
   };
 
   const onSend = () =>{
-    setLines([...lines, text]);
+    //push mesaage to firebase server
+    chatroomRef.push({
+      sender: "Mark : ",
+      message: text,
+      timestamp: firebase.database.ServerValue.TIMESTAMP
+    });
     setText("");
   };
 
@@ -22,26 +45,28 @@ function Chatbox() {
   };
 
   return (
-<App>
-    <div className="App">
-      <div className="App-chatroom">
-        {
-          lines.map(x => {
-            return <div className="App-chatroom-text">
-                    {x}
-                   </div>
-          })
-        }
-      </div>
-      <div className="App-textbox">
-        <input type="text" className="App-textbox-input" 
-        value={text} onChange={onTextChange} onKeyPress={keyPress}/>
+    <App>
+        <div className="App">
+            <div className="App-chatroom">
+                {
+                lines.map(x => {
+                    return <div className="App-chatroom-text">
+                            {x.sender}
+                            {x.message}
+                        </div>
+                    })
+                }
+            </div>
+        <div className="App-textbox">
+            <input type="text" className="App-textbox-input" 
+            value={text} onChange={onTextChange} onKeyPress={keyPress}/>
 
-        <div className="App-textbox-send" onClick={onSend}>Send!</div>
-      </div>
-    </div>
-</App>
+            <div className="App-textbox-send" onClick={onSend}>Send!</div>
+            </div>
+        </div>
+    </App>
   );
 }
 
 export default Chatbox;
+
---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+
+
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAWMp_umgiO9dswcV47PhlsbTwwyucuymE",
+    authDomain: "my-chatapp-1890.firebaseapp.com",
+    projectId: "my-chatapp-1890",
+    storageBucket: "my-chatapp-1890.appspot.com",
+    messagingSenderId: "607927157487",
+    appId: "1:607927157487:web:861623844a62c86ed0511a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase;

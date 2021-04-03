1e0cc8314bcbfa763de14bc29a90280920c65ed5
digitake <digitake@gmail.com>
2021-04-03 16:26:03 +0700

    Prevent lines overflow by adding overflow: auto to css

--- src/ChatFriend.js
+++ src/ChatFriend.js
@@ -1,80 +1,82 @@
 import './App.css';
-import {useState,useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import HeaderOfChat from './HeaderOfChat';
 import firebase from './firebaseConfig';
 
-const chatFriendRef=firebase.database().ref('chatFriend')
+const chatFriendRef = firebase.database().ref('chatFriend')
 
 function ChatFriend() {
   const [text, setText] = useState("");
   const [lines, setLines] = useState([]);
 
-  useEffect(()=>{chatFriendRef.on('child_added',snapshot=>{ let x = snapshot.val();
-   
-    setLines(line=>[...line,{
-      sender:x.sender,
-      message:x.message,
-      timeStamp:new Date(x.timeStamp)
-    }])
+  useEffect(() => {
+    chatFriendRef.on('child_added', snapshot => {
+      let x = snapshot.val();
+
+      setLines(line => [...line, {
+        sender: x.sender,
+        message: x.message,
+        timeStamp: new Date(x.timeStamp)
+      }])
     })
-  },[]);
+  }, []);
 
   const onTextChange = (event) => {
     setText(event.target.value);
   };
 
 
-  const onSend = () =>{
+  const onSend = () => {
     chatFriendRef.push({
-sender:"Me",
-message:text,
-timeStamp:firebase.database.ServerValue.TIMESTAMP
+      sender: "Me",
+      message: text,
+      timeStamp: firebase.database.ServerValue.TIMESTAMP
 
-    });            
+    });
 
-setText("");
-};
+    setText("");
+  };
 
 
-  
-const keyPress = (event) => {               
-  if (event.which === 13){
-    onSend();
-  }
-};
+
+  const keyPress = (event) => {
+    if (event.which === 13) {
+      onSend();
+    }
+  };
 
   return (
     <HeaderOfChat>
-    <div className="App">
-   
-   <div className="App-chatroom">
-     {
-       lines.map(x => {
-         return <div className="App-chatroom-text">
-                  
+      <div className="App">
+
+        <div className="App-chatroom">
+          {
+            lines.map(x => {
+              return <div className="App-chatroom-text">
+
                 <div className="App-chatroom-text-sender">
-                    {x.sender+":"}
+                  {x.sender + ":"}
                 </div>
-                <div className="App-chatroom-text-message"> 
-                  {x.message} 
-                   <div >                      
-                   {" Time "+x.timeStamp.toLocaleString()}
-                   </div>
-                 </div>
+                <div className="App-chatroom-text-message">
+                  {x.message}
+                  <div >
+                    {" Time " + x.timeStamp.toLocaleString()}
+                  </div>
                 </div>
-       })
-     }
-   </div>
-   <div className="App-textbox">
-     <input type="text" className="App-textbox-input" 
-     value={text} onChange={onTextChange} onKeyPress={keyPress}/>
-      
-     <div className="App-textbox-send" onClick={onSend}>Send!</div>
-   
-     
-     
-   </div>
- </div>
+              </div>
+            })
+          }
+        </div>
+        <div className="App-textbox">
+          <input type="text" className="App-textbox-input"
+            value={text} onChange={onTextChange} onKeyPress={keyPress} />
+
+          <div className="App-textbox-send" onClick={onSend}>Send!</div>
+
+
+
+        </div>
+      </div>
     </HeaderOfChat>
   );
 }

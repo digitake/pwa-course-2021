3f6352d9fc496388cb569ed3bed0862d18d34ad7
janjao93 <janjao937@gmail.com>
2021-04-03 15:24:27 +0700

    AddFirebaseInGroupChat

--- src/GroupChat.js
+++ src/GroupChat.js
@@ -1,20 +1,39 @@
 import './App.css';
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
 import HeaderOfChat from './HeaderOfChat';
+import firebase from './firebaseConfig';
 
+
+const chatGroupRef=firebase.database().ref('chatGroup')
 
 function GroupChat() {
   const [text, setText] = useState("");
   const [lines, setLines] = useState([]);
+  
+  useEffect(()=>{chatGroupRef.on('child_added',snapshot=>{ let x = snapshot.val();
+   
+    setLines(line=>[...line,{
+      sender:x.sender,
+      message:x.message,
+      timeStamp:new Date(x.timeStamp)
+    }])
+    })
+  },[]);
 
   const onTextChange = (event) => {
     setText(event.target.value);
   };
 
   const onSend = () =>{
-    setLines([...lines, {People:"Me",message:text}]);
-    setText("");
-  };
+    chatGroupRef.push({
+sender:"Me",
+message:text,
+timeStamp:firebase.database.ServerValue.TIMESTAMP
+
+    });            
+
+setText("");
+};
 
   const keyPress = (event) => {
     if (event.which === 13){
@@ -24,33 +43,36 @@ function GroupChat() {
 
   return (
     <HeaderOfChat>
-    <div className="App">
-    
-     
-      <div className="App-chatroom">
-        {
-          lines.map(x => {
-            return <div className="App-chatroom-text">
-                  <div className="App-Chatroom-sender">  
-                    {x.People+" :"}
-                  </div>
-                  <div className="App-chatroom-messag e"> 
-                   {x.message}
-                 </div>   
+   <div className="App">
+   
+   <div className="App-chatroom">
+     {
+       lines.map(x => {
+         return <div className="App-chatroom-text">
+                  
+                <div className="App-chatroom-text-sender">
+                    {x.sender+":"}
+                </div>
+                <div className="App-chatroom-text-message"> 
+                  {x.message} 
+                   <div >                      
+                   {" Time "+x.timeStamp.toLocaleString()}
+                   </div>
                  </div>
-          })
-        }
-      </div>
-      <div className="App-textbox">
-        <input type="text" className="App-textbox-input" 
-        value={text} onChange={onTextChange} onKeyPress={keyPress}/>
-         
-        <div className="App-textbox-send" onClick={onSend}>Send!</div>
+                </div>
+       })
+     }
+   </div>
+   <div className="App-textbox">
+     <input type="text" className="App-textbox-input" 
+     value={text} onChange={onTextChange} onKeyPress={keyPress}/>
       
-        
-        
-      </div>
-    </div>
+     <div className="App-textbox-send" onClick={onSend}>Send!</div>
+   
+     
+     
+   </div>
+ </div>
     </HeaderOfChat>
   );
 }

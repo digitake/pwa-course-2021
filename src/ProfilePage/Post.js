b8a1aaec95f9236ddf478289b79299d03d2892ed
Noraphat_Thodsadee <noraphat.thod@bumail.net>
2021-04-06 22:20:30 +0700

    เพิ่ม ฟ้อน ,อัพเดต หน้าโพส

--- src/ProfilePage/Post.js
+++ src/ProfilePage/Post.js
@@ -5,9 +5,13 @@ import { Link } from "react-router-dom";
 function Post(){
     return(
     <Profile>
-    <div className="Post">
-        
-           Post
+    <div className="Post"> 
+           <h2>Rimuru</h2>
+           <p>กรุงเทพมหานคร เป็นเมืองหลวงและนครที่มีประชากรมากที่สุดของประเทศไทย เป็นศูนย์กลางการปกครอง 
+            การศึกษา การคมนาคมขนส่ง การเงินการธนาคาร การพาณิชย์ การสื่อสาร และความเจริญของประเทศ</p>
+
+           <h2>Rimuru</h2>
+           <p>Hold fast to dreams For if dreams die Life is a broken-winged bird That cannot fly</p>
     </div>
     </Profile>
     );

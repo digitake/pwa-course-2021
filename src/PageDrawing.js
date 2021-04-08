47921de1c66bc6cf0cce15e4ba58b8d30b2edb69
AnawatThan <Anawat.than@bumail.net>
2021-04-08 15:55:17 +0700

    รอจารย์ช่วยPageDrawing

--- src/PageDrawing.js
+++ src/PageDrawing.js
@@ -1,15 +1,19 @@
 import { Link } from 'react-router-dom';
 import './PageDrawing.css';
 import drawing from './Drawing_Icon.png';
+import Logo from './Logo.png'
+import ChatLogo from './Cahat_Icon.png'
 
 function Page4Drawling() {
   return (
     <div className="PageDrawing">
       <div className="PageDrawing-header">
-        Page Drawing
+        <img src={Logo}></img>
+          Page Drawing
         <div className="PageDrawing-botton">
           <Link to="/App">
-            <button>Go to Chatroom</button>
+          <img src={ChatLogo}></img>
+              
           </Link>
         </div>
       </div>

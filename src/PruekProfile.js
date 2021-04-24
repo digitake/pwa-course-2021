58dfb1fb2e01b4fd672a4a44a02b53f7d09c7bc6
natchaphon_sirisangsawang <natchaphon.siri@bumail.net>
2021-04-24 11:09:26 +0700

    Add my new Profile Because My friend not do that for a long timeeeeeee

---
+++ src/PruekProfile.js
@@ -0,0 +1,28 @@
+import {Link} from "react-router-dom";
+import "./PruekProfile.css";
+
+
+function PruekProfile() {
+    
+    return (
+        // <div className="App col-12">
+        <div>
+            {/*<Link to="/App">*/}
+            {/*    <button>*/}
+            {/*        Back to chat*/}
+            {/*    </button>*/}
+            {/*</Link>*/}
+
+
+            <div className="textpro">Profile</div>
+            <div className="profilePicture"/>
+            
+            
+            
+            
+            
+        </div>
+
+    );
+};
+export default PruekProfile;

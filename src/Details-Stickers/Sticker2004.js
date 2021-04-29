eb8ce32aa72c58bcc47a453db09141bad744b6e8
Khanin_Jittapan <Khanin.jitt@bumail.net>
2021-04-29 20:13:14 +0700

    New file Sticker2004.js : Add base code

---
+++ src/Details-Stickers/Sticker2004.js
@@ -0,0 +1,55 @@
+import './Sticker0001.css';
+import { Link } from "react-router-dom";
+
+function Sticker2004(){
+    return(    
+        <div className="StickerPage">
+            <div className="StickerPage-header">
+                Sticker Details
+                <Link to="/EffectStickersList">
+                    <img src ="back-32.png" align= "Left" />
+                </Link>
+            </div>
+            <div className="StickerPage-body">
+                <div className="StickerPage-profile-sticker">
+                    <div className=".StickerPage-profile-image">
+                        <div className ="Profile-image">
+                            <img src="Profile.png" align="Bottom" />
+                        </div>
+                    </div>
+                    <div className=".StickerPage-profile-name-description">
+                        <div className=".Profile-name">
+                            <p id="name">Name 2004</p>
+                        </div>
+                        <div className=".Profile-description">
+                            <p id="description">Description 2004</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="StickerPage-details-zone">
+                    <div className="StickerPage-details-sticker-header">
+                    </div>
+                    <div className="StickerPage-details-sticker">
+                        <div className=".StickerPage-details-sticker-row">
+                            <img src="Profile-250.png" width="150px" height="150px" />
+                            <img src="Profile-250.png" width="150px" height="150px" />
+                            <img src="Profile-250.png" width="150px" height="150px" />
+                        </div>
+                        <div className=".StickerPage-details-sticker-row">
+                            <img src="Profile-250.png" width="150px" height="150px" />
+                            <img src="Profile-250.png" width="150px" height="150px" />
+                            <img src="Profile-250.png" width="150px" height="150px" />
+                        </div>
+                        <div className=".StickerPage-details-sticker-row">
+                            <img src="Profile-250.png" width="150px" height="150px" />
+                            <img src="Profile-250.png" width="150px" height="150px" />
+                            <img src="Profile-250.png" width="150px" height="150px" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>  
+    );
+};
+
+export default Sticker2004;

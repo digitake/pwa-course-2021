1da926fbc594f80c2c9819a1de49b8a5a7bbb129
Khanin_Jittapan <Khanin.jitt@bumail.net>
2021-04-29 20:06:17 +0700

    Update NewStickersList.js : New Sticker1002-1006

--- src/Sticker-Category/NewStickersList.js
+++ src/Sticker-Category/NewStickersList.js
@@ -20,9 +20,98 @@ function NewStickersList(){
                     </div>
                 </div>
             </div>
-            </Link>
-            
-        </div>  
+            </Link>   
+        </div>
+
+        <div className="StickerList">
+            <Link to ="/Sticker1002">
+                <div className="Sticker-from">
+                <div className="Stcker-ShowImg">
+                    <img src="Profile-250.png" width="90px" height="90px" />
+                </div>
+                <div className="Sticker-DisptionZone">
+                    <div className="Sticker-Name">
+                        <p id="name">Sticker 1002</p>
+                    </div>
+                    <div className="Sticker-Disption">
+                        <p id="disption">Disption 1002</p>
+                    </div>
+                </div>
+            </div>
+            </Link>   
+        </div>
+
+        <div className="StickerList">
+            <Link to ="/Sticker1003">
+                <div className="Sticker-from">
+                <div className="Stcker-ShowImg">
+                    <img src="Profile-250.png" width="90px" height="90px" />
+                </div>
+                <div className="Sticker-DisptionZone">
+                    <div className="Sticker-Name">
+                        <p id="name">Sticker 1003</p>
+                    </div>
+                    <div className="Sticker-Disption">
+                        <p id="disption">Disption 1003</p>
+                    </div>
+                </div>
+            </div>
+            </Link>   
+        </div>
+
+        <div className="StickerList">
+            <Link to ="/Sticker1004">
+                <div className="Sticker-from">
+                <div className="Stcker-ShowImg">
+                    <img src="Profile-250.png" width="90px" height="90px" />
+                </div>
+                <div className="Sticker-DisptionZone">
+                    <div className="Sticker-Name">
+                        <p id="name">Sticker 1004</p>
+                    </div>
+                    <div className="Sticker-Disption">
+                        <p id="disption">Disption 1004</p>
+                    </div>
+                </div>
+            </div>
+            </Link>   
+        </div>
+
+        <div className="StickerList">
+            <Link to ="/Sticker1005">
+                <div className="Sticker-from">
+                <div className="Stcker-ShowImg">
+                    <img src="Profile-250.png" width="90px" height="90px" />
+                </div>
+                <div className="Sticker-DisptionZone">
+                    <div className="Sticker-Name">
+                        <p id="name">Sticker 1005</p>
+                    </div>
+                    <div className="Sticker-Disption">
+                        <p id="disption">Disption 1005</p>
+                    </div>
+                </div>
+            </div>
+            </Link>   
+        </div>
+
+        <div className="StickerList">
+            <Link to ="/Sticker1006">
+                <div className="Sticker-from">
+                <div className="Stcker-ShowImg">
+                    <img src="Profile-250.png" width="90px" height="90px" />
+                </div>
+                <div className="Sticker-DisptionZone">
+                    <div className="Sticker-Name">
+                        <p id="name">Sticker 1006</p>
+                    </div>
+                    <div className="Sticker-Disption">
+                        <p id="disption">Disption 1006</p>
+                    </div>
+                </div>
+            </div>
+            </Link>   
+        </div>
     </ShopPage>                  
     );
 };

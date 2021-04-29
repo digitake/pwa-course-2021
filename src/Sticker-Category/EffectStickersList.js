1aab65286286042702216916e7608c4d9c37833a
Khanin_Jittapan <Khanin.jitt@bumail.net>
2021-04-29 20:18:05 +0700

    Update EffectStickersList.js: Add Sticker2002-2005

--- src/Sticker-Category/EffectStickersList.js
+++ src/Sticker-Category/EffectStickersList.js
@@ -20,9 +20,80 @@ function EffectStickersList(){
                     </div>
                 </div>
             </div>
-            </Link>
-            
-        </div>  
+            </Link>    
+        </div>
+
+        <div className="StickerList">
+            <Link to ="/Sticker2002">
+                <div className="Sticker-from">
+                <div className="Stcker-ShowImg">
+                    <img src="Profile-250.png" width="90px" height="90px" />
+                </div>
+                <div className="Sticker-DisptionZone">
+                    <div className="Sticker-Name">
+                        <p id="name">Sticker 2002</p>
+                    </div>
+                    <div className="Sticker-Disption">
+                        <p id="disption">Disption 2002</p>
+                    </div>
+                </div>
+            </div>
+            </Link>    
+        </div>
+        
+        <div className="StickerList">
+            <Link to ="/Sticker2003">
+                <div className="Sticker-from">
+                <div className="Stcker-ShowImg">
+                    <img src="Profile-250.png" width="90px" height="90px" />
+                </div>
+                <div className="Sticker-DisptionZone">
+                    <div className="Sticker-Name">
+                        <p id="name">Sticker 2003</p>
+                    </div>
+                    <div className="Sticker-Disption">
+                        <p id="disption">Disption 2003</p>
+                    </div>
+                </div>
+            </div>
+            </Link>    
+        </div>
+        
+        <div className="StickerList">
+            <Link to ="/Sticker2004">
+                <div className="Sticker-from">
+                <div className="Stcker-ShowImg">
+                    <img src="Profile-250.png" width="90px" height="90px" />
+                </div>
+                <div className="Sticker-DisptionZone">
+                    <div className="Sticker-Name">
+                        <p id="name">Sticker 2004</p>
+                    </div>
+                    <div className="Sticker-Disption">
+                        <p id="disption">Disption 2004</p>
+                    </div>
+                </div>
+            </div>
+            </Link>    
+        </div>
+        
+        <div className="StickerList">
+            <Link to ="/Sticker2005">
+                <div className="Sticker-from">
+                <div className="Stcker-ShowImg">
+                    <img src="Profile-250.png" width="90px" height="90px" />
+                </div>
+                <div className="Sticker-DisptionZone">
+                    <div className="Sticker-Name">
+                        <p id="name">Sticker 2005</p>
+                    </div>
+                    <div className="Sticker-Disption">
+                        <p id="disption">Disption 2005</p>
+                    </div>
+                </div>
+            </div>
+            </Link>    
+        </div>
     </ShopPage>                  
     );
 };

87cb08da7f58b7157270941a24ce28faf767e04f
Khanin_Jittapan <Khanin.jitt@bumail.net>
2021-04-29 16:00:30 +0700

    Update Router.js : Add Route path Sticker3001

--- src/Router.js
+++ src/Router.js
@@ -13,6 +13,7 @@ import HotStickersList from "./Sticker-Category/HotStickersList";
 import Sticker0001 from "./Details-Stickers/Sticker0001";
 import Sticker1001 from "./Details-Stickers/Sticker1001";
 import Sticker2001 from "./Details-Stickers/Sticker2001";
+import Sticker3001 from "./Details-Stickers/Sticker3001";
 import NewStickersList from "./Sticker-Category/NewStickersList";
 import EffectStickersList from "./Sticker-Category/EffectStickersList";
 import AnimatedStickersList from "./Sticker-Category/AnimatedStickersList";
@@ -86,6 +87,10 @@ function Router()  {
 
             <Route path="/Sticker2001">
               <Sticker2001 />
+            </Route>
+
+            <Route path="/Sticker3001">
+              <Sticker3001 />
             </Route>
 
             <Route path="/">

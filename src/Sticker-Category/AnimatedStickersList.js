25c4331714b5c60f51796438d78f0c25cbbe9d8a
Khanin_Jittapan <Khanin.jitt@bumail.net>
2021-04-29 20:31:21 +0700

    Update AnimatedStickersList.js : New Sticker3002-3004

--- src/Sticker-Category/AnimatedStickersList.js
+++ src/Sticker-Category/AnimatedStickersList.js
@@ -21,8 +21,61 @@ function AnimatedStickersList(){
                 </div>
             </div>
             </Link>
-            
-        </div>  
+        </div>
+
+        <div className="StickerList">
+            <Link to ="/Sticker3002">
+                <div className="Sticker-from">
+                <div className="Stcker-ShowImg">
+                    <img src="Profile-250.png" width="90px" height="90px" />
+                </div>
+                <div className="Sticker-DisptionZone">
+                    <div className="Sticker-Name">
+                        <p id="name">Sticker 3002</p>
+                    </div>
+                    <div className="Sticker-Disption">
+                        <p id="disption">Disption 3002</p>
+                    </div>
+                </div>
+            </div>
+            </Link>
+        </div>
+        
+        <div className="StickerList">
+            <Link to ="/Sticker3003">
+                <div className="Sticker-from">
+                <div className="Stcker-ShowImg">
+                    <img src="Profile-250.png" width="90px" height="90px" />
+                </div>
+                <div className="Sticker-DisptionZone">
+                    <div className="Sticker-Name">
+                        <p id="name">Sticker 3003</p>
+                    </div>
+                    <div className="Sticker-Disption">
+                        <p id="disption">Disption 3003</p>
+                    </div>
+                </div>
+            </div>
+            </Link>
+        </div>
+        
+        <div className="StickerList">
+            <Link to ="/Sticker3004">
+                <div className="Sticker-from">
+                <div className="Stcker-ShowImg">
+                    <img src="Profile-250.png" width="90px" height="90px" />
+                </div>
+                <div className="Sticker-DisptionZone">
+                    <div className="Sticker-Name">
+                        <p id="name">Sticker 3004</p>
+                    </div>
+                    <div className="Sticker-Disption">
+                        <p id="disption">Disption 3004</p>
+                    </div>
+                </div>
+            </div>
+            </Link>
+        </div>
     </ShopPage>                  
     );
 };

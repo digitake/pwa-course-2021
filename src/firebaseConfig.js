427ff05855c45ce089731ac7cd1ab8031d053ec8
klongkwan <klongkwan2000@gmail.com>
2021-05-01 09:15:11 +0700

    xe

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,14 +4,14 @@ import  firebase from 'firebase';
 var firebaseConfig = {
     apiKey: "AIzaSyCLirnobt_7UBWtuPkB8UII4cN-77KvCpg",
     authDomain: "bu-chatapp-pwa-chatclub.firebaseapp.com",
-    databaseURL: "https://chatapp-pwa-course-1620703478-default-rtdb.firebaseio.com",
+    databaseURL: "https://nottae12-pwa-default-rtdb.firebaseio.com/",
     projectId: "bu-chatapp-pwa-chatclub",
     storageBucket: "bu-chatapp-pwa-chatclub.appspot.com",
     messagingSenderId: "706022678488",
-    appId: "1:706022678488:web:59a5ede4a64792a2459cad"
+    appId: "1:706022678488:web:e174e7aad4e1f2bf459cad"
 };
 
-// Initialize Firebase
+  // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 export default firebase;

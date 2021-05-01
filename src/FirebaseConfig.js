ae1cb7a9c56900c50f82be029c5e59462a2f86a3
OosnowoO <61759530+OosnowoO@users.noreply.github.com>
2021-05-01 10:23:28 +0700

    Fixed code

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -1,16 +1,17 @@
 import firebase from 'firebase';
 
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+  apiKey: "AIzaSyAHi_UOpCJuml_u0WR39R2svjUOvFTymuQ",
+  authDomain: "chatapp-b5a54.firebaseapp.com",
+  databaseURL: "https://chatapp-b5a54-default-rtdb.firebaseio.com",
+  projectId: "chatapp-b5a54",
+  storageBucket: "chatapp-b5a54.appspot.com",
+  messagingSenderId: "782307115129",
+  appId: "1:782307115129:web:1662846640741e75e614a8"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-var firebaseConfig = {
-    apiKey: "AIzaSyAHi_UOpCJuml_u0WR39R2svjUOvFTymuQ",
-    authDomain: "chatapp-b5a54.firebaseapp.com",
-    databaseURL: "https://chatapp-b5a54-default-rtdb.firebaseio.com",
-    projectId: "chatapp-b5a54",
-    storageBucket: "chatapp-b5a54.appspot.com",
-    messagingSenderId: "782307115129",
-    appId: "1:782307115129:web:1662846640741e75e614a8"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
 
   export default firebase;

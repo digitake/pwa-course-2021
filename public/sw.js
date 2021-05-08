1dfb1c47d0162a9d89a6c62df61fba3498a9ff2d
ThanatipLnwza <thanatip.ngan@bumail.net>
2021-05-08 10:42:21 +0700

    add semico

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,17 @@
 
 self.addEventListener("install",(event) => {
     console.log("[SW] Service Worker is installing...v1.1");
+    
     event.waitUntil(
+
         caches. open('chatapp-v1.1')
+        
         .then((cache) =>{
+            
             return cache.addAll([
                 '/',
                 '/index.html'
-            ])
+            ]);
         })
-    )
-})
+    );
+});

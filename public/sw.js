92becced632642e6a8377d357c12201ab60e517f
natchaphon_sirisangsawang <natchaphon.siri@bumail.net>
2021-05-08 11:00:27 +0700

    Make MyApp to PWA

---
+++ public/sw.js
@@ -0,0 +1,29 @@
+//Test service Worker install
+
+self.addEventListener("install", (event) => 
+{
+    console.log("[SW] Service Worker is installing.... v1.2");
+    
+    event.waitUntil(
+        caches.open('chatapp-v1.2')
+            .then((cache) => {
+                return cache.addAll([
+                    '/',
+                    "/index.html"
+                    ]);
+            })
+    );
+});
+
+
+self.addEventListener('fetch',event => {
+    caches.match(event.request)
+        .then(res => {
+            if(res) {
+                console.log("Intercept with cache",res);
+                return res
+            }
+            
+            return fetch(event.request);
+        })
+})
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Router from './Router';
@@ -15,3 +15,14 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('sw.js')
+        .then(function(registeration){
+            
+            console.log('Registration successful, scope is :',registeration.scope);
+        }).catch(function (error){
+            console.log('Service worker registration failed,error',error);
+    })
+}

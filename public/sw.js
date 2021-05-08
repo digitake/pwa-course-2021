d4ce3ede018faddf970452799b19cf2e01069ebc
ThanatipLnwza <thanatip.ngan@bumail.net>
2021-05-08 10:02:26 +0700

    add cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,13 @@
 
 self.addEventListener("install",(event) => {
-    console.log("[SW] Service Worker is installing...");
+    console.log("[SW] Service Worker is installing...v1.1");
+    event.waitUntil(
+        caches. open('chatapp-v1.1')
+        .then((cache) =>{
+            return cache.addAll([
+                '/',
+                '/index.html'
+            ])
+        })
+    )
 })

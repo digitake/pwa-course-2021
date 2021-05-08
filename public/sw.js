d9e54541086f014897bdb8edf35d2a86f40f2c2e
Khanin_Jittapan <Khanin.jitt@bumail.net>
2021-05-08 11:26:18 +0700

    Update Service Wokers And App-PWA Do Inclass8/5/21

---
+++ public/sw.js
@@ -0,0 +1,29 @@
+self.addEventListener("install", (event) =>{
+    console.log("[SW] Service Worker is installing...v1.1");
+
+    event.waitUntil(
+      caches.open('chatapp-v1.1')
+        .then((caches) => {
+
+            return caches.addAll([
+                '/',
+                '/index.html'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+
+    caches.match(event.request.url)
+    .then(res => {
+        if(res) {
+          console.log("Intercept with cache", res);
+          return res
+        }
+  
+        return fetch(event.request);
+      }
+    )
+  
+});  

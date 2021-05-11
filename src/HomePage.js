3199c1f4d0801c39b70be83dec069f9356cb2bf9
klongkwan <klongkwan2000@gmail.com>
2021-05-11 22:18:31 +0700

    n1

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,14 +1,10 @@
 import { Link } from 'react-router-dom';
-
 import App from './App';
 
-import FriendListphoto from './image/friendlist.png'
-
-import chatroomphoto from './image/chatroom.png'
-
-import profilephoto from './image/profile.png'
-
-import addfriendphoto from './image/addfriend.png'
+import chatroomphoto from './image/home.png';
+import profilephoto from './image/user.png';
+import Addfriendphoto from './image/addfriend.png';
+import FriendListphoto from './image/users_1.png';
 
 function HomePage() {
     return (
@@ -16,19 +12,19 @@ function HomePage() {
         <div>
 
             <Link to="/friend-list">
-                <img src={FriendListphoto} width="400" />
+                <img src={FriendListphoto} width="400"></img>
             </Link>
 
             <Link to="/">
-                <img src={chatroomphoto} width="400" />
+                <img src={chatroomphoto} width="400"></img>
             </Link>
 
             <Link to="/profile">
-                <img src={profilephoto} width="400" />
+                <img src={profilephoto} width="400"></img>
             </Link>
 
             <Link to="/add-friend">
-                <img src={addfriendphoto} width="400" />
+                <img src={Addfriendphoto} width="400" />
             </Link>
             
         </div>

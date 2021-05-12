7b269f1efbd41b2c1fcfee303dd09aed6d0a5e5d
NattaponRP <nattapon.rung@bumail.net>
2021-05-12 13:34:32 +0700

    add caches + deploy

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,30 @@
 self.addEventListener("install", (event) => {
-    console.log("[SW] Service Worker is installing...");
+    console.log("[SW] Service Worker is installing... v1.2");
+    
+    event.waitUntil(
+        
+        caches.open('chatapp-v1.2')
+        .then((caches) => {
+
+            return caches.addAll([
+                '/',
+                '/index.html'
+            ]);
+        })
+
+    );
+});
+
+
+self.addEventListener('fetch', (event) =>{
+    caches.match(event.request)
+    .then(res => {
+        if(res){
+            console.log("Intercept with cache", res);
+            return res
+        }
+
+        return fetch(event.request);
+    })
 })
 

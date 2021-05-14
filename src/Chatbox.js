e6322b43f3ea8894070594884eda902c5eeef851
natchaphon_sirisangsawang <natchaphon.siri@bumail.net>
2021-05-14 17:26:59 +0700

    เเก้ OverFlow ทั้งหมด ***คนเดียว*** Debug ที่ถ้าไม่ใส่ชื่อจะทำให้ App Error + เพิ่ม Text fill name + edit name input position and size

--- src/Chatbox.js
+++ src/Chatbox.js
@@ -1,19 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import './Chatbox.css';
 import LobbyHeader from "./LobbyHeader"
 import firebase from "./firebaseConfig";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 const chatroomRef = firebase.database().ref('fantasma-room-1');
 
 
-function ChatBox({ children }) {
+function ChatBox({children}) {
 
     const [text, setText] = useState("");
 
     const [lines, setLines] = useState([]);
-    
-    const [name, setName] = useState();
+
+    const [name, setName] = useState("");
 
 
     // () => {} same as function() {}  --lambda notation
@@ -31,7 +31,7 @@ function ChatBox({ children }) {
     const onTextChange = (event) => {
         setText(event.target.value);
     };
-    
+
     const onNameChange = (event) => {
         setName(event.target.value);
     };
@@ -54,39 +54,43 @@ function ChatBox({ children }) {
 
     return (
         <LobbyHeader>
-    <div className="App col-12">
-            <input type="text" placeholder="Please Input Your Name" value={name} onChange={onNameChange}/> 
-                    <div className="App-chatroom">
-                        {
-                            lines.map(x => {
-                                return <div className="App-chatroom-text">
-                                    <div className="user"></div>
-                                    <div className="App-textPosition">
-                                        {x.sender + " : "}
-                                        {x.message}
-                                    </div>
-                                    <div className="App-Time-Position"> {x.timestamp.toLocaleTimeString()} </div>
-                                </div>
-                            })
-                        }
-                    </div>
-                    {children}
-            {/*ChatBar Input*/}
-            <div className="App-textbox">
-                <div className="App-Botton col-1">
-                    <Link to="/LobbySetting">
-                        <div className="App-inButtonBar "> Setting </div>
-                    </Link>
+            <div className="App col-12">
+                <div className="App-namebox">
+                    <input type="text" placeholder="Please Input Your Name" value={name} onChange={onNameChange}/>
+                    " Your Name Here " === >
                 </div>
-                <input type="text" placeholder="Type Ready to Start!" className="App-textbox-input col-11"
-                    value={text}
-                    onChange={onTextChange}
-                    onKeyPress={keyPress} />
+                <div className="App-chatroom">
+                    {
+                        lines.map(x => {
+                            return <div className="App-chatroom-text">
+                                <div className="user"></div>
+                                <div className="App-textPosition">
+                                    {x.sender + " : "}
+                                    {x.message}
+                                </div>
+                                <div className="App-Time-Position"> {x.timestamp.toLocaleTimeString()} </div>
+                            </div>
+                        })
+                    }
+                </div>
+                {children}
+                
+                {/*ChatBar Input*/}
+                <div className="App-textbox">
+                    <div className="App-Botton col-1">
+                        <Link to="/LobbySetting">
+                            <div className="App-inButtonBar "> Setting</div>
+                        </Link>
+                    </div>
+                    <input type="text" placeholder="Type Ready to Start!" className="App-textbox-input col-11"
+                           value={text}
+                           onChange={onTextChange}
+                           onKeyPress={keyPress}/>
+                </div>
             </div>
-        </div>
         </LobbyHeader>
-
     );
 }
+//Message form 1620701795 Give Me A SenPai
 
 export default ChatBox;

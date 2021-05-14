17bed94c631a922d24dfa272ab69094f56fcb878
thanachot1620702660 <thanachot.chai@bumail.net>
2021-05-14 23:09:46 +0700

    MainPage Complete

--- src/Main.js
+++ src/Main.js
@@ -8,20 +8,48 @@ function Main(){
         <App>
             <div>
                 <p></p>
+                
                 <div className="header">    
-                 Profile        
+                   <p1>CHEN WICK</p1> 
                     <Link to="friendlist">
-                        <img src="เณรเชน.jpg"width= "30%" height="100%" /> 
+                        <img src="เณรเชน.jpg"width= "30%" height="100%" align="right"/> 
                     </Link>
                 </div>
+                <p></p>
+                
+                <input type="text" placeholder="Search.." name="search2"></input>
 
+                <p></p>
+                <div className="friendlistcontent">
+                    friendlist
+                <div className="friendlistbox">
+                    <img src="mark.png" width="50px"/>
+                    MARK
+                </div>
                 
+                <div className="friendlistbox">
+                    <img src="mon.png"/>
+                    MON
+                </div> 
                 
+                <div className="friendlistbox">
+                    <img src="tin.png"/>
+                    TIN
+                </div>
                 
+                <div className="friendlistbox">
+                    <img src="domne.jpg"/>
+                    DOME
+                </div>
+
+                </div>
                 
+                <p></p>
+                <div className="bottom">
+                    News
+                </div>                     
                 
-                
-                
+    
                        
             </div>
         </App>

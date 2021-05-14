dcbf076cd0645df16eea34f90b672f1e9e1ea190
Ridakami <77868807+Ridakami@users.noreply.github.com>
2021-05-14 12:13:49 +0700

    Original

--- src/index.js
+++ src/index.js
@@ -16,3 +16,11 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js').then(function (registration) {
+    console.log('Regidtration successful, scope is:', registration.scope);
+  }).catch(function (error){
+    console.log('Service worker registration failed, error', error)
+  })
+}

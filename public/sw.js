9f5595977e1b768a6addbd029dd636cc110e3d4b
nottae12 <pannathat.lima@bumail.net>
2021-05-14 16:34:29 +0700

    up

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,13 @@
 
 self.addEventListener('install' , event =>{
-    console.log("[SW] Service worker is installing . . .")
+    console.log("[SW] Service worker is installing . . .v3")3
+
+    event.waitUntil(
+        caches.open('chatapp-v3').then ( Cache => {
+            return Cache.addAll([
+            '/',
+            '/index.html'
+            ]);
+        })
+    );
 });

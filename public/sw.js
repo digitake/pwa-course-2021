80552b619b783d413c83e3b29e4ba8b631961014
boonywdee <boonywdee.saku@bumail.net>
2021-05-14 22:40:26 +0700

    [งานเดี่ยว]ทำให้เป็น PWA ได้แล้ว

---
+++ public/sw.js
@@ -0,0 +1,28 @@
+
+self.addEventListener("install", (event) =>{
+    console.log("[SW] Service Worker is installing...v1.1");
+
+    event.waitUntil(
+        caches.open('chatapp-v1.1').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/logo192.png',
+                '/logo512.png'
+            ]);
+        })
+    );
+});
+
+
+self.addEventListener('fetch', event => {
+    caches.match(event.request.url)
+    .then(res => {
+        if(res) {
+            console.log("Intercept with cache", res);
+            return res
+        }
+        return fetch(event.request);
+      } 
+    )
+})
--- src/index.js
+++ src/index.js
@@ -16,3 +16,14 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js')
+  .then(function(registration) {
+    console.log('Registration successful, scope is:', registration.scope);
+
+  }).catch(function(error){
+    console.log('Service woeker registration failed, error', error);
+
+  })
+}

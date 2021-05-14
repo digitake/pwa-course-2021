1d7efc7087471f044678fb84ece0db0bcc79307c
Nattakan-phim <Nattakan.phim@gmail.net>
2021-05-15 02:36:18 +0700

    Commit Pwa app

--- src/HomePage.js
+++ src/HomePage.js
@@ -29,34 +29,34 @@ function HomePage({children}) {
             <div className = "pandding">
 
                 <Link to = "/Chatroom">
-                <img src = "ChatBoxBar.Png" />
+                <img src = "ChatBoxBar.Png" alt  = "ChatBoxBar.Png" />
                 </Link>
             </div>
 
             <div className = "pandding" >
 
                 <Link to = "/GroupChatRoom">
-                    <img src = "GroupChat.Png" />
+                    <img src = "GroupChat.Png"  alt = "GroupChat.Png" />
                 </Link>
             </div>
 
             <div className = "HomePageInfo-topBar-HomePageButton">
 
                 <Link to = "/HomePage">
-                <img src = "Home.png" />
+                <img src = "Home.png" alt = "Home.png"/>
                 </Link> 
             </div>
 
             <div className = "pandding">
 
                 <Link to = "/PrivatePage">
-                    <img src = "Private.Png"  />
+                    <img src = "Private.Png" alt = "Private.Png" />
                 </Link>
             </div>
             <div className = "pandding">
 
                 <Link to = "/SettingPage">
-                    <img src = "SettingBar.Png" />
+                    <img src = "SettingBar.Png" alt = "SettingBar.Png" />
                 </Link>
             </div>
         

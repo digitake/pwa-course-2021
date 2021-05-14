d4f97d57f7d7747b1e8cd4e5ae647f60f0feb04a
Thanaphat1150 <thanaphat.sang@bumail.net>
2021-05-14 11:22:18 +0700

    Add Pwa

--- public/sw.js
+++ public/sw.js
@@ -1,26 +1,48 @@
-self.addEventListener("install", (event) =>{
+self.addEventListener("install",(event)=>{
     console.log("[SW] Service Worker is installing... v1.1");
 
     event.waitUntil(
-        caches.open('chatapp-v1.1')
+        caches.open('chatapp-v1.2')
         .then((caches) => {
             return caches.addAll([
                 "/",
                 "/index.html",
-                "/logoname.png"
+                "/logo Icon.png",
+                "/add new.png",
+                "armprofile.png",
+                "back.png",
+                "cat.png",
+                "choice.png",
+                "dice message logo.png",
+                "dice.png",
+                "friend list.png",
+                "info.png",
+                "list.png",
+                "login icon.png",
+                "plus black no bg.png",
+                "plus no bg.png",
+                "plus.png",
+                "ppp.jpg",
+                "profile.png",
+                "register icon.png",
+                "rut.png",
+                "search.png",
+                "share.png",
+                "spin.png",
+                "logoIcon192.png"
             ]);
         })
     );
 });
 
-self.addEventListener('fetch', event =>{
-    caches.match(event.request)
+self.addEventListener('fetch', event => {
+    caches.match(event.request.url)
     .then(res => {
         if(res) {
             console.log("Intercept with cache", res);
             return res
         }
-
         return fetch(event.request);
-    })
+      }
+    )
 });

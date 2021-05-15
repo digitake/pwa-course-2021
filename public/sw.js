e3db6096100773b8311e0417ac34d68fae7596f9
Napasin <napasin.chon@bumail.net>
2021-05-15 10:00:42 +0700

    Can install now

--- public/sw.js
+++ public/sw.js
@@ -41,17 +41,16 @@ self.addEventListener("install", (event) =>{
     );
 });
 
-self.addEventListener('fecth', (event) =>{
-
-    caches.match (event.request)
-    .then(res => {
-        
-        if(res) {
-            console.log("Intercept with cache", res);
-            return res
+self.addEventListener('fetch', function(event) {
+    event.respondWith(async function() {
+       try{
+         var res = await fetch(event.request);
+         var cache = await caches.open('cache');
+         cache.put(event.request.url, res.clone());
+         return res;
+       }
+       catch(error){
+         return caches.match(event.request);
         }
-        
-        return fetch(event.request);
-    })
-    
-});
+      }());
+  });
